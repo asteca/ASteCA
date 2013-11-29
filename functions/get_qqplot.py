31f0b873cea046c84b2ebfbc2a5cc09eba17b15e
Gabriel-p <gabrielperren@gmail.com>
2013-11-29 14:14:11 -0300

    add qqplot function file

---
+++ functions/get_qqplot.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 29 14:11:22 2013
+
+@author: gabriel
+"""
+
+'''
+Calculate the QQ-plot for the distribution of p-values obtained comparing
+the cluster's KDE with the field region's KDEs.
+'''
+
+def qqplot():
+    
