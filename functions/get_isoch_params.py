3276814c8b2842296b8fc6c10a5640c8a3d52da8
Gabriel-p <gabrielperren@gmail.com>
2014-01-09 17:04:49 -0300

    functions position rearrange 2

--- functions/get_isoch_params.py
+++ functions/get_isoch_params.py
@@ -64,6 +64,50 @@ def get_ranges_paths(sys_select):
     dis_mod_step, iso_path, line_start, mag_index
     
     
+
+def move_track(sys_select, dis_mod, e_bv, iso_list, age_val):
+    '''
+    Recieves an isochrone of a given age and metallicity and modifies
+    it according to given values for the extinction E(B-V) and distance
+    modulus.
+    '''
+    iso_moved = [[], []]
+    
+    if sys_select == '1':
+        # For UBVI system.
+        #
+        # E(B-V) = (B-V) - (B-V)o
+        # Av = 3.1*E(B-V)
+        # (mv - Mv)o = -5 + 5*log(d) + Av
+        #
+        Av = 3.1*e_bv
+        for item in iso_list[age_val][1]:
+            # mv affected by extinction.
+            iso_moved[1].append(item + dis_mod + Av)
+        for item in iso_list[age_val][0]:
+            # (B-V) affected by extinction.
+            iso_moved[0].append(item + e_bv)
+    else:
+        # For Washington system.
+        #
+        # E(C-T1) = 1.97*E(B-V) = (C-T1) - (C-T)o
+        # M_T1 = T1 + 0.58*E(B-V) - (m-M)o - 3.2*E(B-V)
+        #
+        # (C-T1) = (C-T1)o + 1.97*E(B-V)
+        # T1 = M_T1 - 0.58*E(B-V) + (m-M)o + 3.2*E(B-V)
+        #
+        V_Mv = dis_mod + 3.2*e_bv
+        for item in iso_list[age_val][1]:
+             # T1 magnitude affected by extinction.
+            iso_moved[1].append(item - 0.58*e_bv + V_Mv)
+        for item in iso_list[age_val][0]:
+             # C-T1 color affected by extinction.
+            iso_moved[0].append(item + 1.97*e_bv)    
+
+    return iso_moved[0], iso_moved[1]    
+    
+    
+    
 def read_isochrones(sys_select):
     '''
     Reads and stores all isochrones available inside the indicated folder.
@@ -154,48 +198,6 @@ def read_isochrones(sys_select):
     return isochrones, isoch_params
 
 
-
-def move_track(sys_select, dis_mod, e_bv, iso_list, age_val):
-    '''
-    Recieves an isochrone of a given age and metallicity and modifies
-    it according to given values for the extinction E(B-V) and distance
-    modulus.
-    '''
-    iso_moved = [[], []]
-    
-    if sys_select == '1':
-        # For UBVI system.
-        #
-        # E(B-V) = (B-V) - (B-V)o
-        # Av = 3.1*E(B-V)
-        # (mv - Mv)o = -5 + 5*log(d) + Av
-        #
-        Av = 3.1*e_bv
-        for item in iso_list[age_val][1]:
-            # mv affected by extinction.
-            iso_moved[1].append(item + dis_mod + Av)
-        for item in iso_list[age_val][0]:
-            # (B-V) affected by extinction.
-            iso_moved[0].append(item + e_bv)
-    else:
-        # For Washington system.
-        #
-        # E(C-T1) = 1.97*E(B-V) = (C-T1) - (C-T)o
-        # M_T1 = T1 + 0.58*E(B-V) - (m-M)o - 3.2*E(B-V)
-        #
-        # (C-T1) = (C-T1)o + 1.97*E(B-V)
-        # T1 = M_T1 - 0.58*E(B-V) + (m-M)o + 3.2*E(B-V)
-        #
-        V_Mv = dis_mod + 3.2*e_bv
-        for item in iso_list[age_val][1]:
-             # T1 magnitude affected by extinction.
-            iso_moved[1].append(item - 0.58*e_bv + V_Mv)
-        for item in iso_list[age_val][0]:
-             # C-T1 color affected by extinction.
-            iso_moved[0].append(item + 1.97*e_bv)    
-
-    return iso_moved[0], iso_moved[1]
-    
 
 
 def gip(memb_prob_avrg_sort):
