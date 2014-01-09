e38589af371c7970dfdec8276b121ffcb3c7968f
Gabriel-p <gabrielperren@gmail.com>
2014-01-09 16:54:21 -0300

    move move_track to new func

--- functions/get_isoch_params.py
+++ functions/get_isoch_params.py
@@ -67,6 +67,49 @@ def get_ranges_paths():
     
     
 
+def move_track(sys_select, dis_mod, e_bv, iso_list, age_val):
+    '''
+    Recieves an iosochrone of a given age and metallicity and modifies
+    it according to given values for the extinction E(B-V) and distance
+    modulus.
+    '''
+    iso_moved = [[], []]
+    
+    if sys_select == '1':
+        # For UBVI system.
+        #
+        # E(B-V) = (B-V) - (B-V)o
+        # Av = 3.1*E(B-V)
+        # (mv - Mv)o = -5 + 5*log(d) + Av
+        #
+        Av = 3.1*e_bv
+        for item in iso_list[age_val][1]:
+            # mv affected by extinction.
+            iso_moved[1].append(item + dis_mod + Av)
+        for item in iso_list[age_val][0]:
+            # (B-V) affected by extinction.
+            iso_moved[0].append(item + e_bv)
+    else:
+        # For Washington system.
+        #
+        # E(C-T1) = 1.97*E(B-V) = (C-T1) - (C-T)o
+        # M_T1 = T1 + 0.58*E(B-V) - (m-M)o - 3.2*E(B-V)
+        #
+        # (C-T1) = (C-T1)o + 1.97*E(B-V)
+        # T1 = M_T1 - 0.58*E(B-V) + (m-M)o + 3.2*E(B-V)
+        #
+        V_Mv = dis_mod + 3.2*e_bv
+        for item in iso_list[age_val][1]:
+             # T1 magnitude affected by extinction.
+            iso_moved[1].append(item - 0.58*e_bv + V_Mv)
+        for item in iso_list[age_val][0]:
+             # C-T1 color affected by extinction.
+            iso_moved[0].append(item + 1.97*e_bv)    
+
+    return iso_moved[0], iso_moved[1]    
+    
+    
+
 def read_isochrones(iso_path, line_start, mag_index):
     '''
     Reads and stores all isochrones available in the indicated folder.
@@ -133,7 +176,8 @@ def read_isochrones(iso_path, line_start, mag_index):
             for age_val in range(age_indx+1):
                 
                 # Move isochrone according to E(B-V) and dis_mod values thus
-                # generating a new function. Store this function in 'funcs' list.
+                # generating a new function. Store this function in
+                # 'isochrones' list.
                 for e_bv in np.arange(e_bv_min, e_bv_max, e_bv_step):
                     
                     for dis_mod in np.arange(dis_mod_min, dis_mod_max,
@@ -144,8 +188,8 @@ def read_isochrones(iso_path, line_start, mag_index):
                                              round(e_bv, 2), round(dis_mod, 2)])
                         
                         # Call function to move track.
-                        iso_moved_0, iso_moved_1 = move_track(sys_select, dis_mod,
-                                                              e_bv) 
+                        iso_moved_0, iso_moved_1 = move_track(sys_select, \
+                        dis_mod, e_bv, iso_list, age_val) 
                         # Append final x and y CMD data to array.
                         isochrones.append([iso_moved_0, iso_moved_1])
                     
