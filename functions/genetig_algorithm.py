2cba3ba92b8fded197a00757fa6d230b745d4786
Gabriel-p <gabrielperren@gmail.com>
2014-01-17 10:04:10 -0300

    laying out the work to be done in the gen algor

--- functions/genetig_algorithm.py
+++ functions/genetig_algorithm.py
@@ -10,3 +10,34 @@ def gen_algor():
     '''
     Genetic algorithm adapted to find the best fit model-obervation.
     '''
+    
+    # Translate space of all possible solutions to binary format.
+    
+    # Pick N solutions (genotypes/chromosomes) at random
+    
+    
+    ### Evaluation/fitness ###
+    
+    # Evaluate the N random chromosomes in the evaluation/objective function
+    # (likelihood)
+    
+    # Calculate fitness value for each chromosome.
+    
+    
+    #### Selection ###
+    # Generate CDF for the chromosome's probability distribution (fitness).
+    
+    # Select N chromosomes for breeding from the CDF to generate the
+    # intermediate population.
+    
+    
+    #### Breeding ### 
+    
+    # Apply crossover operation on random chromosomes.
+    
+    # Apply mutation operation on random chromosomes.
+    
+    
+    ### Decoding###
+    
+    # Translate binary form to normal naming to process this new generation.
