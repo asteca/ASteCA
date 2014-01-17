193d030f80f576d715d31847519d6db9c9117e2a
Gabriel-p <gabrielperren@gmail.com>
2014-01-17 09:44:23 -0300

    add genetic algor function file

---
+++ functions/genetig_algorithm.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 17 09:43:14 2014
+
+@author: gabriel
+"""
+
+
+def gen_algor():
+    '''
+    Genetic algorithm adapted to find the best fit model-obervation.
+    '''
