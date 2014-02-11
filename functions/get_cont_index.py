6dbf8b6873e229a4318f1828e729a21e07c513f8
Gabriel-p <gabrielperren@gmail.com>
2014-02-11 18:38:57 -0300

    cont index, no changes

--- functions/get_cont_index.py
+++ functions/get_cont_index.py
@@ -5,26 +5,25 @@ Created on Thu Nov 21 15:51:34 2013
 @author: gabriel
 """
 
-'''
-Calculate and return the contamination index value cont_index.
-The contamination index is defined as the ratio of field stars that should 
-be present in the cluster region (calculated by means of the background value
-'backg_val' and the cluster's radius 'r') to the total number of stars in the
-cluster region 'n_tot'.
-
-CI = [backg_val*PI*r**2]/n_tot
-
-If this number equals 1, it means that all of the stars in the cluster region
-are expected to be field stars. A small number (close to zero) means the
-field contamination in the cluster region is very small. A number larger
-than 1 means there are more stars in average in the background than there are
-inside the cluster region (which isn't a good sign).
-'''
-
 import numpy as np
 
 def cont_indx(backg_val, clust_rad, stars_in, stars_in_rjct):
+    '''
+    Calculate and return the contamination index value cont_index.
+    The contamination index is defined as the ratio of field stars that should 
+    be present in the cluster region (calculated by means of the background value
+    'backg_val' and the cluster's radius 'r') to the total number of stars in the
+    cluster region 'n_tot'.
     
+    CI = [backg_val*PI*r**2]/n_tot
+    
+    If this number equals 1, it means that all of the stars in the cluster region
+    are expected to be field stars. A small number (close to zero) means the
+    field contamination in the cluster region is very small. A number larger
+    than 1 means there are more stars in average in the background than there are
+    inside the cluster region (which isn't a good sign).
+    '''
+   
     # Cluster's area.
     a_c = np.pi*(clust_rad**2)
     
