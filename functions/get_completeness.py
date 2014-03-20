cb3ff5d46eaa4dde9d008a2d46d2501fa3117b56
Gabriel-p <gabrielperren@gmail.com>
2014-03-20 17:48:56 -0300

    PEP8 on get_completeness

--- functions/get_completeness.py
+++ functions/get_completeness.py
@@ -7,15 +7,16 @@ Created on Fri Jan 24 10:37:16 2014
 
 import numpy as np
 
+
 def mag_completeness(mag_data):
     '''
     Calculate the completeness level in each magnitude bin beyond the one
     with the maximum count (ie: the assumed 100% completeness limit)
     '''
     # Max value of magnitude.
-    max_mag = max(mag_data)    
+    max_mag = max(mag_data)
     # Number of bins.
-    bins = int((max(mag_data)-min(mag_data))/0.1)
+    bins = int((max(mag_data) - min(mag_data)) / 0.1)
     # Get histogram.
     mag_hist, bin_edges = np.histogram(mag_data, bins)
     # Index of the bin with the maximum number of stars.
@@ -28,9 +29,9 @@ def mag_completeness(mag_data):
     # These values tell me the percentages of stars beyond the magnitude peak
     # that are located inside each magnitude bin. The peak magnitude bin (the
     # first one) will have the biggest percentage.
-    comp_perc = [(i*100.)/total for i in mag_hist[max_indx:]]
-    
+    comp_perc = [(i * 100.) / total for i in mag_hist[max_indx:]]
+
     # Store everything in a single list.
     completeness = [max_mag, bin_edges, max_indx, comp_perc]
-    
+
     return completeness
