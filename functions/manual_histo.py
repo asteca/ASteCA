9d7e35d7f914ae036b657bfa7d1542e9438ed9f0
Gabriel-p <gabrielperren@gmail.com>
2014-03-25 17:38:24 -0300

    PEP8 on manual_histo

--- functions/manual_histo.py
+++ functions/manual_histo.py
@@ -1,14 +1,11 @@
-#import matplotlib.pyplot as plt
-#import matplotlib.gridspec as gridspec
 
 
 def manual_histo(phot_data, xedges, yedges):
-    
     ''' Obtains a (manual) 2D histogram for the field with not only the number
     of stars per bin but also the values associated to each of those stars: x, y
     coordinates and magnitude, e_mag, color, e_color values.
     '''
-    
+
     id_star, x_data, y_data, mag_data, e_mag, col1_data, e_col1 = phot_data
 
     # Create the empty list that will hold the information of each star that
@@ -16,13 +13,13 @@ def manual_histo(phot_data, xedges, yedges):
     # H = [ [[num of stars in bin], [star 1 data (id,x,y,T1, etc..)],
     # [star 2 data], [star 3 data], ...], [], [], ..., [] ]
     H_manual = []
-    for i in range(len(xedges)-1):
-        H_manual.append([[0] for _ in xrange(len(yedges)-1)])
-    
+    for i in range(len(xedges) - 1):
+        H_manual.append([[0] for _ in xrange(len(yedges) - 1)])
+
     # Iterate through all the stars in the frame.
     for xindex, xcoord in enumerate(x_data):
         ycoord = y_data[xindex]
-        
+
         # Iterate through all edges in the x axis.
         for xbin_index, xitem_edg in enumerate(xedges):
 
@@ -30,7 +27,7 @@ def manual_histo(phot_data, xedges, yedges):
             if xcoord >= xitem_edg:
                 try:
                 # Check to see if a bin beyond this one exists.
-                    xedges[xbin_index+1]
+                    xedges[xbin_index + 1]
                 except IndexError:
                 # Bin doesn't exist so the star is located at the border of the
                 # x axis.
@@ -41,7 +38,7 @@ def manual_histo(phot_data, xedges, yedges):
                         if ycoord >= yitem_edg:
                             try:
                             # Check to see a bin beyond this one exists.
-                                yedges[ybin_index+1]
+                                yedges[ybin_index + 1]
                             except IndexError:
                             # Star located at the border of the y axis.
                                 try:
@@ -56,9 +53,9 @@ def manual_histo(phot_data, xedges, yedges):
                                 # Store the star's data and increase the count
                                 # for this bin.
                                     H_manual[xbin_index][ybin_index][0] += 1
-                                    H_manual[xbin_index][ybin_index].extend(\
-                                    [[id_star[xindex], xcoord, ycoord, \
-                                    mag_data[xindex], e_mag[xindex], \
+                                    H_manual[xbin_index][ybin_index].extend(
+                                    [[id_star[xindex], xcoord, ycoord,
+                                    mag_data[xindex], e_mag[xindex],
                                     col1_data[xindex], e_col1[xindex]]])
                                 break
                             else:
@@ -66,7 +63,7 @@ def manual_histo(phot_data, xedges, yedges):
                             # the border of the y axis)
                                 # Check if star is contained by top border in y
                                 # axis.
-                                if ycoord < yedges[ybin_index+1]:
+                                if ycoord < yedges[ybin_index + 1]:
                                     try:
                                     # Check if the bin exists.
                                         H_manual[xbin_index][ybin_index]
@@ -77,23 +74,23 @@ def manual_histo(phot_data, xedges, yedges):
                                     # It does, store star's data and increase
                                     # the count for this bin.
                                         H_manual[xbin_index][ybin_index][0] += 1
-                                        H_manual[xbin_index][ybin_index].extend(\
-                                        [[id_star[xindex], xcoord, ycoord,\
-                                        mag_data[xindex], e_mag[xindex],\
+                                        H_manual[xbin_index][ybin_index].extend(
+                                        [[id_star[xindex], xcoord, ycoord,
+                                        mag_data[xindex], e_mag[xindex],
                                         col1_data[xindex], e_col1[xindex]]])
                                     break
                 else:
                 # A bin beyond this one in the x axis exists so star is not in
                 # the border.
                     # Check if star is contained by right border in x axis.
-                    if xcoord < xedges[xbin_index+1]:
+                    if xcoord < xedges[xbin_index + 1]:
                         # Iterate through all edges in the y axis.
                         for ybin_index, yitem_edg in enumerate(yedges):
 
                             # Check if star falls inside this bin in the y axis.
                             if ycoord >= yitem_edg:
                                 try:
-                                    yedges[ybin_index+1]
+                                    yedges[ybin_index + 1]
                                 except IndexError:
                                     # Star located in the border of the y axis.
                                     try:
@@ -104,50 +101,53 @@ def manual_histo(phot_data, xedges, yedges):
                                         pass
                                     else:
                                         H_manual[xbin_index][ybin_index][0] += 1
-                                        H_manual[xbin_index][ybin_index].extend(\
-                                        [[id_star[xindex], xcoord, ycoord, \
-                                        mag_data[xindex], e_mag[xindex], \
+                                        H_manual[xbin_index][ybin_index].extend(
+                                        [[id_star[xindex], xcoord, ycoord,
+                                        mag_data[xindex], e_mag[xindex],
                                         col1_data[xindex], e_col1[xindex]]])
                                     break
                                 else:
-                                    if ycoord < yedges[ybin_index+1]:
+                                    if ycoord < yedges[ybin_index + 1]:
                                         try:
                                             H_manual[xbin_index][ybin_index]
                                         except IndexError:
                                             pass
                                         else:
-                                            H_manual[xbin_index][ybin_index][0] \
+                                            H_manual[xbin_index][ybin_index][0]\
                                             += 1
                                             H_manual[xbin_index][ybin_index].\
-                                            extend([[id_star[xindex], xcoord, \
-                                            ycoord, mag_data[xindex], \
-                                            e_mag[xindex], col1_data[xindex], \
+                                            extend([[id_star[xindex], xcoord,
+                                            ycoord, mag_data[xindex],
+                                            e_mag[xindex], col1_data[xindex],
                                             e_col1[xindex]]])
                                         break
-    
-    
+
     return H_manual
-   
+
 # Check to see if all items are equal. A couple of bins in the borders will
 # probaly not have the same values.
 #    for xindex, xelem in enumerate(H):
 #        for yindex, yelem in enumerate(xelem):
 #            if yelem != H_manual[xindex][yindex][0]:
-#                print myfile, xindex, yindex, yelem, H_manual[xindex][yindex][0]
-#              
+#                print myfile, xindex, yindex, yelem,
+#                    H_manual[xindex][yindex][0]
+#
 #    print H[25][37], H_manual[25][37]
 #
-#    # Plot both 2D histograms to check that they are equal  
+#    # Plot both 2D histograms to check that they are equal
 #    H_manual2 = []
 #    for i in range(len(xedges)-1):
 #        H_manual2.append([0 for _ in xrange(len(yedges)-1)])
 #    for xindex, xelem in enumerate(H_manual):
 #        for yindex, yelem in enumerate(xelem):
 #            H_manual2[xindex][yindex] =  H_manual[xindex][yindex][0]
-#    
+#
+# import matplotlib.pyplot as plt
+# import matplotlib.gridspec as gridspec
+#
 #    fig = plt.figure(figsize=(10, 5)) # create the top-level container
 #    gs = gridspec.GridSpec(1, 2)
-#    plt.subplot(gs[0, 0])    
+#    plt.subplot(gs[0, 0])
 #    plt.imshow(H.transpose(), origin='lower')
 #    plt.subplot(gs[0, 1])
 #    plt.imshow(np.array(H_manual2).transpose(), origin='lower')
