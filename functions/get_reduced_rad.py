0dccb4ec7bc2cacd1fa4489471ec8291ec6f1db2
Gabriel-p <gabrielperren@gmail.com>
2014-04-04 21:15:47 -0300

    almost finished with reduced_radius func

--- functions/get_reduced_rad.py
+++ functions/get_reduced_rad.py
@@ -18,44 +18,39 @@ def red_rad(flag_red_rad, backg_val, clust_rad, cont_index, center_cl,
 
     radii, ring_density = rdp_params[:2]
 
-    new_rad, flag_new_rad = clust_rad, False
+    flag_new_rad = False
+    indx_rad = min(range(len(radii)), key=lambda i: abs(radii[i] - clust_rad))
+    new_rad = radii[indx_rad]
+
     i = 1
-    if flag_red_rad is True:
+    if flag_red_rad is True and cont_index > 0.5:
 
-        stars_in = [0. for _ in memb_prob_avrg_sort]
-        while cont_index > 0.5 and len(stars_in) > \
-        0.05 * len(memb_prob_avrg_sort):
-            # Reduce radius by 1 px.
-            new_rad = clust_rad - i
+        # The condition (indx_rad - i) > 0 means that the minimum radius
+        # value is the one associated with the second point in the raddi
+        # list.
+        while flag_new_rad is False and (indx_rad - i) > 0:
 
-            stars_in, stars_in_rjct = [], []
-            for star in memb_prob_avrg_sort:
-                dist = np.sqrt((center_cl[0] - star[1]) ** 2 +
-                    (center_cl[1] - star[2]) ** 2)
-                if dist <= new_rad:
-                    stars_in.append(0.)
-
-            print new_rad, backg_val, len(stars_in), np.pi * (new_rad ** 2), \
-            (float(len(stars_in)) / np.pi * (new_rad ** 2))
-
-            # Get new cont_indx with new radius value.
-            cont_index = ci(backg_val, new_rad, stars_in, stars_in_rjct)
-            if cont_index <= 0.5:
-                flag_new_rad = True
+            # Reduce radius.
+            new_rad = radii[indx_rad - i]
             i += 1
-            print cont_index
+            # Get new cont_indx with new radius value.
+            cont_index = ci(backg_val, rdp_params, new_rad)
 
-        if flag_new_rad is True:
-            memb_prob_avrg_sort2 = []
-            for star in memb_prob_avrg_sort:
-                dist = np.sqrt((center_cl[0] - star[1]) ** 2 +
-                    (center_cl[1] - star[2]) ** 2)
-                if dist <= new_rad:
-                    memb_prob_avrg_sort2.append(star)
+            if cont_index <= 0.5:
+                # Obtain new members.
+                memb_prob_avrg_sort2 = []
+                for star in memb_prob_avrg_sort:
+                    dist = np.sqrt((center_cl[0] - star[1]) ** 2 +
+                        (center_cl[1] - star[2]) ** 2)
+                    if dist <= new_rad:
+                        memb_prob_avrg_sort2.append(star)
+                # Break out of while.
+                flag_new_rad = True
+                print 'Reduced radius found (%0.2f).' % new_rad
 
-    else:
+    if flag_red_rad is not True or flag_new_rad is not True:
+        print 'No reduced radius value found.'
+        # Return unchanged list of members.
         memb_prob_avrg_sort2 = memb_prob_avrg_sort
 
-    print len(memb_prob_avrg_sort), len(memb_prob_avrg_sort2)
-    raw_input()
     return memb_prob_avrg_sort2
