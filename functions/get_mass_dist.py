e3b6222735a3ed0b23be5fc042a66050c7e9fbd5
Gabriel-p <gabrielperren@gmail.com>
2014-07-22 18:17:51 -0300

    rename some variables for better reading

--- functions/get_mass_dist.py
+++ functions/get_mass_dist.py
@@ -11,29 +11,31 @@ Created on Wed Jan 15 15:26:39 2014
 import numpy as np
 
 
-def weighted_fast_samp(refs, weights, lim):
+def weighted_fast_samp(mass_init, probs, M_total):
 
     # Sample the masses with replacement according to the probabilities
     # given to each by the PDF.
     blocksize = 10000
-    samp = np.random.choice(refs, size=blocksize, replace=True, p=weights)
-    samp_sum = np.cumsum(samp)
+    masses = np.random.choice(mass_init, size=blocksize, replace=True, p=probs)
+    # Sum all the random masses selected (CDF)
+    masses_sum = np.cumsum(masses)
 
-    # Is the sum of our current block of samples >= lim?
-    while samp_sum[-1] < lim:
+    # Is the sum of our current block of masses >= M_total?
+    while masses_sum[-1] < M_total:
 
         # Sample another block.
-        newsamp = np.random.choice(refs, size=blocksize, replace=True,
-                                   p=weights)
+        newsamp = np.random.choice(mass_init, size=blocksize, replace=True,
+                                   p=probs)
         # Stack arrays in sequence horizontally (column wise).
-        samp = np.hstack((samp, newsamp))
-        # Update sum of masses.
-        samp_sum = np.hstack((samp_sum, np.cumsum(newsamp) + samp_sum[-1]))
+        masses = np.hstack((masses, newsamp))
+        # Update sum of masses and go check again.
+        masses_sum = np.hstack((masses_sum, np.cumsum(newsamp) +
+            masses_sum[-1]))
 
-    # Find index where the lim value is found in samp_sum.
-    last = np.searchsorted(samp_sum, lim, side='right')
+    # Find index where the M_total value is found in samp_sum.
+    last = np.searchsorted(masses_sum, M_total, side='right')
 
-    return samp[:last + 1]
+    return masses[:last + 1]
 
 
 def mass_dist(mass_params):
@@ -41,9 +43,9 @@ def mass_dist(mass_params):
     Returns a mass distribution according to a given IMF and a total cluster
     mass.
     '''
-    imf_cdf, M_total = mass_params
+    imf_pdf, M_total = mass_params
     # Sample masses from IMF according to the PDF obtained from it until
     # the sum of all stars reaches M_total.
-    dist_mass = weighted_fast_samp(imf_cdf[0], imf_cdf[1], M_total)
+    dist_mass = weighted_fast_samp(imf_pdf[0], imf_pdf[1], M_total)
 
     return dist_mass
