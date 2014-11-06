4e0af4936c2f24381ad2a924ca7b6e1553e0b543
Gabriel-p <gabrielperren@gmail.com>
2014-11-06 12:26:43 -0300

    Revert "fix bug where plot couldnt be produced if DA run but BF did not"
    This reverts commit 2a74b45d2d78303c17b3633dca6f8ded77ae3a9f.

--- functions/synth_cl_err.py
+++ functions/synth_cl_err.py
@@ -55,12 +55,11 @@ def synth_clust_err(phot_data, err_pck):
     cluster generation function.
     '''
 
-    da_flag, bf_flag = g.da_params[0], g.bf_params[0]
+    bf_flag = g.bf_params[0]
     err_lst = []
 
-    # Check if function should run. If DA was executed, we obtain these
-    # values for ploting purposes only.
-    if da_flag != 'skip' or bf_flag:
+    # Check if algorithm should run.
+    if bf_flag:
 
         # Unpack params. Use *main* magnitude.
         mag, e_mag, e_col = phot_data[0][0], phot_data[1], phot_data[3]
