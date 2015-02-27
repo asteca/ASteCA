228216217bb16dcce58f843c38e1821320f733d9
Gabriel-p <gabrielperren@gmail.com>
2015-02-27 14:56:05 -0300

    typos in checker

--- functions/checker.py
+++ functions/checker.py
@@ -142,7 +142,7 @@ def check(mypath, cl_files):
         if not isfile(join(mypath, semi_file)):
             # File semi_input.dat does not exist.
             sys.exit("ERROR: 'semi' mode is set but semi_input.dat file does"
-                "not exist.")
+                " not exist.")
 
     # Check px/deg.
     if g.gd_params[-1] not in {'px', 'deg'}:
@@ -158,22 +158,22 @@ def check(mypath, cl_files):
     if g.pl_params[0] is True:
         if g.pl_params[1] not in {'png', 'pdf', 'PNG', 'PDF'}:
             sys.exit("ERROR: figure output format selected ('{}') is"
-            "not valid.".format(g.pl_params[1]))
+            " not valid.".format(g.pl_params[1]))
 
     # 2D positional histogram.
     if g.gh_params[0] not in {'auto', 'manual'}:
         sys.exit("ERROR: mode selected ('{}') for 2D histogram"
-        "is not valid.".format(g.gh_params[0]))
+        " is not valid.".format(g.gh_params[0]))
 
     # Radius finding function.
     if g.cr_params[0] not in {'low', 'mid', 'high'}:
         sys.exit("ERROR: mode selected ('{}') for radius finding"
-        "function is not valid.".format(g.cr_params[0]))
+        " function is not valid.".format(g.cr_params[0]))
 
     # Errors function.
     if g.er_params[0] not in {'emax', 'lowexp', 'eyefit'}:
         sys.exit("ERROR: mode selected ('{}') for error rejecting"
-        "function is not valid.".format(g.er_params[0]))
+        " function is not valid.".format(g.er_params[0]))
     if g.er_params[0] == 'emax' and len(g.er_params[1:]) < 1:
         sys.exit("ERROR: missing parameters for error rejecting function")
     if g.er_params[0] == 'eyefit' and len(g.er_params[1:]) < 3:
