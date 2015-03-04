3195c00bdac0400f56855d5dcf8c9cb99cdf2dfd
Gabriel-p <gabrielperren@gmail.com>
2015-03-04 11:52:23 -0300

    close #169

--- functions/exp_function.py
+++ functions/exp_function.py
@@ -9,11 +9,11 @@ def exp_3p(x, a, b, c):
     '''
     Three-params exponential function.
     '''
-    return a * np.exp(b * x) + c
+    return a * np.exp(b * np.asarray(x)) + c
 
 
 def exp_2p(x, a, b):
     '''
     Two-params exponential function.
     '''
-    return a * np.exp(x) + b
+    return a * np.exp(np.asarray(x)) + b
