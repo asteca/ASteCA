176d490c2e6adaa4bdaf8e329117d6e7d90aa540
Gabriel-p <gabrielperren@gmail.com>
2015-03-25 09:45:50 -0300

    manual mode, invert when RA, correct axis names, correct star sizes

--- functions/structure/display_frame.py
+++ functions/structure/display_frame.py
@@ -2,8 +2,8 @@
 @author: gabriel
 """
 
-import numpy as np
 import matplotlib.pyplot as plt
+from ..out import prep_plots
 
 
 def disp_frame(x_data, y_data, mag_data):
@@ -11,22 +11,26 @@ def disp_frame(x_data, y_data, mag_data):
     Show full frame.
     '''
 
+    coord, x_name, y_name = prep_plots.coord_syst()
+    st_sizes_arr = prep_plots.star_size(mag_data)
+
     # Get max and min values in x,y
     x_min, x_max = min(x_data), max(x_data)
     y_min, y_max = min(y_data), max(y_data)
     #Set plot limits
     plt.xlim(x_min, x_max)
     plt.ylim(y_min, y_max)
+    if coord == 'deg':
+        # If RA is used, invert axis.
+        plt.gca().invert_xaxis()
     #Set axis labels
-    plt.xlabel('x (pixel)', fontsize=12)
-    plt.ylabel('y (pixel)', fontsize=12)
+    plt.xlabel('{} ({})'.format(x_name, coord), fontsize=12)
+    plt.ylabel('{} ({})'.format(y_name, coord), fontsize=12)
     # Set minor ticks
     plt.minorticks_on()
     # Set grid
     plt.grid(b=True, which='major', color='k', linestyle='-', zorder=1)
     plt.grid(b=True, which='minor', color='k', linestyle='-', zorder=1)
-    st_sizes_arr = 0.1 + 100. * 10 ** ((np.array(mag_data) -
-        min(mag_data)) / -2.5)
     plt.scatter(x_data, y_data, marker='o', c='black', s=st_sizes_arr)
 
     plt.draw()
