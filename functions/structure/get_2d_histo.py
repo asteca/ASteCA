332a1d31a3b685fedc139efef20c18bfafdd42a1
Gabriel-p <gabrielperren@gmail.com>
2015-04-18 22:45:23 -0300

    minor modif in cmmts

--- functions/structure/get_2d_histo.py
+++ functions/structure/get_2d_histo.py
@@ -5,7 +5,7 @@ from .._in import get_in_params as g
 
 def get_2d_histo(x_data, y_data):
     '''
-    Return list of 2D histograms for the positional data.
+    Return 2D histogram for the positional data.
     '''
 
     xmin, xmax = min(x_data), max(x_data)
@@ -20,10 +20,10 @@ def get_2d_histo(x_data, y_data):
     else:
         bin_width = g.gh_params[1]
 
-    # Number of bins in x,y given the bin width 'd_b'
+    # Number of bins in x,y given the bin width.
     binsxy = [int(x_rang / bin_width), int(y_rang / bin_width)]
 
-    # hist is the 2D histogram, *edges store the edges of the bins.
+    # hist_2d is the 2D histogram, *edges store the edges of the bins.
     hist_2d, xedges, yedges = np.histogram2d(x_data, y_data,
         range=[[xmin, xmax], [ymin, ymax]], bins=binsxy)
 
