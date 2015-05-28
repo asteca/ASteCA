30c71ac9933bb281f4b62a72fa6a35df16a3faae
Gabriel-p <gabrielperren@gmail.com>
2015-05-28 14:40:32 -0300

    working on 205

--- functions/out/mp_best_fit.py
+++ functions/out/mp_best_fit.py
@@ -15,15 +15,15 @@ from .._in import get_in_params as g
 
 
 def pl_bf_synth_cl(gs, x_min_cmd, x_max_cmd, y_min_cmd, y_max_cmd, x_ax, y_ax,
-    synth_clst, syn_b_edges, cp_r, cp_e, shift_isoch):
+                   synth_clst, syn_b_edges, cp_r, cp_e, shift_isoch):
     '''
     Best fit synthetic cluster obtained.
     '''
     ax = plt.subplot(gs[4:6, 8:10])
-    #Set plot limits
+    # Set plot limits
     plt.xlim(x_min_cmd, x_max_cmd)
     plt.ylim(y_min_cmd, y_max_cmd)
-    #Set axis labels
+    # Set axis labels
     plt.xlabel('$' + x_ax + '$', fontsize=18)
     plt.ylabel('$' + y_ax + '$', fontsize=18)
     # Set minor ticks
@@ -39,32 +39,40 @@ def pl_bf_synth_cl(gs, x_min_cmd, x_max_cmd, y_min_cmd, y_max_cmd, x_ax, y_ax,
             ax.axhline(y_ed, linestyle=':', color='k', zorder=1)
         # Add text box
         text = '$({};\,{})$'.format(g.bf_params[2], g.bf_params[3])
-        ob = offsetbox.AnchoredText(text, pad=.2, loc=2, prop=dict(size=12))
+        ob = offsetbox.AnchoredText(text, pad=.2, loc=1, prop=dict(size=12))
         ob.patch.set(alpha=0.85)
         ax.add_artist(ob)
     else:
         ax.grid(b=True, which='major', color='gray', linestyle='--', lw=1,
-            zorder=1)
+                zorder=1)
     if synth_clst.any():
         # Plot synth clust.
         plt.scatter(synth_clst[0], synth_clst[2], marker='o', s=40,
                     c='#4682b4', lw=0.5)
-        text1 = '\n$N_{{synth}} = {}$\n'.format(len(synth_clst[0]))
+        text1 = '$N_{{synth}} = {}$'.format(len(synth_clst[0]))
     else:
-        text1 = '\n$N_{{synth}} = {}$\n'.format(0)
+        text1 = '$N_{{synth}} = {}$'.format(0)
     # Add text box
-    text2 = '$z = {} \pm {}$\n'.format(cp_r[0], cp_e[0])
-    text3 = '$log(age) = {} \pm {}$\n'.format(cp_r[1], cp_e[1])
-    text4 = '$E_{{(B-V)}} = {} \pm {}$\n'.format(cp_r[2], cp_e[2])
-    text5 = '$(m-M)_o = {} \pm {}$\n'.format(cp_r[3], cp_e[3])
-    text6 = '$M_{{\odot}} = {} \pm {}$\n'.format(cp_r[4], cp_e[4])
-    text7 = '$b_{{frac}} = {} \pm {}$'.format(cp_r[5], cp_e[5])
-    text = text1 + text2 + text3 + text4 + text5 + text6 + text7
-    ob = offsetbox.AnchoredText(text, pad=.2, loc=1, prop=dict(size=12))
+    ob = offsetbox.AnchoredText(text1, pad=.2, loc=2, prop=dict(size=14))
     ob.patch.set(alpha=0.85)
     ax.add_artist(ob)
     # Plot isochrone.
     plt.plot(shift_isoch[0], shift_isoch[1], 'r', lw=1.2)
+
+    # Add text box to the right of the synthetic cluster.
+    ax_t = plt.subplot(gs[4:6, 10:12])
+    ax_t.axis('off')  # Remove axis from frame.
+    text1 = 'Synthetic cluster parameters:\n\n'
+    text2 = '$z\,\,\,\,\,\,\,\,\,=\, {} \pm {}$\n'.format(cp_r[0], cp_e[0])
+    text3 = '$log(age)\, =\, {} \pm {}$\n'.format(cp_r[1], cp_e[1])
+    text4 = '$E_{{(B-V)}}\,\,\, =\, {} \pm {}$\n'.format(cp_r[2], cp_e[2])
+    text5 = '$(m-M)_o =\, {} \pm {}$\n'.format(cp_r[3], cp_e[3])
+    text6 = '$M_{{\odot}}\,\,\,\,\,\,=\, {} \pm {}$\n'.format(cp_r[4], cp_e[4])
+    text7 = '$b_{{frac}}\,\,\,\,\,\, =\, {} \pm {}$'.format(cp_r[5], cp_e[5])
+    text = text1 + text2 + text3 + text4 + text5 + text6 + text7
+    ob = offsetbox.AnchoredText(text, pad=1, loc=6, prop=dict(size=13))
+    ob.patch.set(alpha=0.85)
+    ax_t.add_artist(ob)
 
 
 def pl_ga_lkl(gs, l_min_max, lkl_old, model_done, new_bs_indx):
@@ -86,7 +94,7 @@ def pl_ga_lkl(gs, l_min_max, lkl_old, model_done, new_bs_indx):
     plt.ylabel('Likelihood', fontsize=12)
     # Add text box.
     text1 = '$N_{total} = %.2e\,;\,N_{btst} = %d$' '\n' % \
-    (len(model_done[0]), N_b)
+        (len(model_done[0]), N_b)
     text2 = '$n_{gen}=%d\,;\,n_{pop}=%d$' '\n' % (n_gen, n_pop)
     text3 = '$f_{dif}=%0.2f\,;\,cr_{sel}=%s$' '\n' % (fdif, cr_sel)
     text4 = '$p_{cross}=%0.2f\,;\,p_{mut}=%0.2f$' '\n' % (p_cross, p_mut)
@@ -98,9 +106,9 @@ def pl_ga_lkl(gs, l_min_max, lkl_old, model_done, new_bs_indx):
 
     # Plot likelihood minimum and mean lines.
     ax.plot(range(len(lkl_old[0])), lkl_old[0], lw=1., c='black',
-              label='$L_{min}$')
+            label='$L_{min}$')
     ax.plot(range(len(lkl_old[0])), lkl_old[1], lw=1., c='blue',
-              label='$L_{mean}$')
+            label='$L_{mean}$')
     # Plot line marking a new best solution found.
     for lin in new_bs_indx:
         lw_lin = 2. if lin > 0.05 * len(lkl_old[0]) else 0.5
@@ -108,7 +116,7 @@ def pl_ga_lkl(gs, l_min_max, lkl_old, model_done, new_bs_indx):
     # Legend.
     handles, labels = ax.get_legend_handles_labels()
     leg = ax.legend(handles, labels, loc='upper left', numpoints=1,
-        fontsize=12)
+                    fontsize=12)
     leg.get_frame().set_alpha(0.7)
 
 
@@ -153,7 +161,7 @@ def pl_2_param_dens(gs, _2_params, min_max_p, cp_r, cp_e, model_done):
         # Plot ellipse error.
         plt.gca()
         ellipse = Ellipse(xy=(xp, yp), width=2 * e_xp, height=2 * e_yp,
-                                edgecolor=cp, fc='None', lw=1.)
+                          edgecolor=cp, fc='None', lw=1.)
         ax.add_patch(ellipse)
     # Else plot an error bar in the corresponding dimension.
     elif e_xp < 0. and e_yp > 0.:
@@ -162,7 +170,7 @@ def pl_2_param_dens(gs, _2_params, min_max_p, cp_r, cp_e, model_done):
         plt.errorbar(xp, yp, xerr=e_xp, color=cp)
     # Plot density map.
     hist, xedges, yedges = np.histogram2d(zip(*model_done[0])[mx],
-        zip(*model_done[0])[my], bins=100)
+                                          zip(*model_done[0])[my], bins=100)
     # H_g is the 2D histogram with a gaussian filter applied
     h_g = gaussian_filter(hist, 2, mode='constant')
     plt.imshow(h_g.transpose(), origin='lower',
@@ -209,12 +217,12 @@ def pl_lkl_dens(gs, ld_p, l_max, lkl_old, min_max_p, cp_r, cp_e, model_done):
     ax.add_artist(ob)
     # Generate 2D Gaussian histogram.
     hist, xedges, yedges = np.histogram2d(zip(*model_done[0])[cp],
-        model_done[1], bins=100)
+                                          model_done[1], bins=100)
     # H_g is the 2D histogram with a gaussian filter applied
     h_g = gaussian_filter(hist, 2, mode='constant')
     plt.imshow(h_g.transpose(), origin='lower', extent=[xedges[0], xedges[-1],
-        yedges[0], yedges[-1]], cmap=plt.get_cmap('gist_yarg'),
-        aspect='auto')
+               yedges[0], yedges[-1]], cmap=plt.get_cmap('gist_yarg'),
+               aspect='auto')
     plt.axvline(x=xp, linestyle='--', color='red', zorder=3)
     if e_xp > 0.:
         # Plot error bars only if errors where assigned.
@@ -251,6 +259,6 @@ def plot(N, *args):
     try:
         fxn(*args)
     except:
-        #import traceback
-        #print traceback.format_exc()
+        # import traceback
+        # print traceback.format_exc()
         print("  WARNING: error when plotting {}.".format(plt_map.get(N)[1]))
