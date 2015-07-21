3d04bb5247e001cf033a3df47e9f89e21c9dd2e5
Gabriel-p <gabrielperren@gmail.com>
2015-07-21 18:00:05 -0300

    check if pip is installed (old python versions) + search for installed packages globally

--- functions/checker.py
+++ functions/checker.py
@@ -6,7 +6,6 @@ Created on Tue Sep 22 09:00:00 2014
 """
 
 from update_check import updater
-import pip
 from os.path import join, isfile, isdir
 import sys
 import traceback
@@ -17,18 +16,30 @@ def pack_check():
     '''
     Check if packages are installed.
     '''
-    inst_packgs = pip.get_installed_distributions()
-    inst_packgs_lst = ["%s" % (i.key) for i in inst_packgs]
-    missing_pckg = []
-    for pckg in ['numpy', 'matplotlib', 'scipy', 'astroml', 'scikit-learn']:
-        if pckg not in inst_packgs_lst:
-            missing_pckg.append(pckg)
+    # Wrap in try statement to catch if 'pip' is not installed.
+    try:
+        import pip
+        # check for globally installed packages.
+        inst_packgs = pip.get_installed_distributions(local_only=False)
+        inst_packgs_lst = ["%s" % (i.key) for i in inst_packgs]
+        missing_pckg = []
+        for pckg in ['numpy', 'matplotlib', 'scipy', 'astroml',
+                     'scikit-learn']:
+            if pckg not in inst_packgs_lst:
+                missing_pckg.append(pckg)
 
-    if missing_pckg:
-        print "ERROR: the following packages are missing:\n"
-        for p in missing_pckg:
-            print " - {}".format(p)
-        sys.exit("\nInstall with: pip install <package>\n")
+        if missing_pckg:
+            print "ERROR: the following packages are missing:\n"
+            for p in missing_pckg:
+                print " - {}".format(p)
+            sys.exit("\nInstall with: pip install <package>\n")
+    except ImportError:
+        # Python versions 2.7.7 onward apparently have 'pip' included by
+        # default, so this check should become obsolete.
+        print("  WARNING: 'pip' is not present. Can't check for installed"
+              " packages.\n")
+        # Return empty list.
+        inst_packgs_lst = []
 
     return inst_packgs_lst
 
