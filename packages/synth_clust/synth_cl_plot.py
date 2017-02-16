b69d2bb7a8dd4ad77e6f60ef301be4fee6f73e82
Gabriel-p <gabrielperren@gmail.com>
2017-02-16 15:32:34 -0300

    generate synth cluster and moved isochrone

--- packages/synth_clust/synth_cl_plot.py
+++ packages/synth_clust/synth_cl_plot.py
@@ -3,15 +3,14 @@ import move_isochrone
 import synth_cluster
 
 
-def main(ip_list, isoch_fit_params, err_lst, completeness,
-         st_dist_mass, e_max, bin_mass_ratio, cmd_sel):
+def main(e_max, bin_mr, fundam_params, theor_tracks, isoch_fit_params, err_lst,
+         completeness, st_dist_mass, ext_coefs, N_fc):
     '''
     # Generate shifted isochrone and synthetic cluster for plotting.
     '''
-    # Get list of stored isochrones and their parameters.
-    isoch_list, fundam_params = ip_list[0], ip_list[1]
     # Read best fit values for all parameters.
-    m, a, e, d, mass, binar_f = isoch_fit_params[0]
+    synth_cl_params = isoch_fit_params[0]
+    m, a, e, d, mass, binar_f = synth_cl_params
     # Find indexes for metallicity and age. If indexes are not found due
     # to some difference in the significant figures, use the indexes
     # [0, 0] to prevent the code from halting.
@@ -19,11 +18,14 @@ def main(ip_list, isoch_fit_params, err_lst, completeness,
         m_i, a_i = fundam_params[0].index(m), fundam_params[1].index(a)
     except:
         m_i, a_i = [0, 0]
+        print("  WARNING: metallicity and age for best match synthetic\n"
+              "  cluster not found.")
     # Generate shifted best fit isochrone.
-    shift_isoch = move_isochrone.main(isoch_list[m_i][a_i][:2], e, d, cmd_sel)
+    isochrone = theor_tracks[m_i][a_i]
+    shift_isoch = move_isochrone.main(isochrone, e, d, ext_coefs, N_fc)
     # Generate best fit synthetic cluster.
     synth_clst = synth_cluster.main(
-        err_lst, completeness, st_dist_mass, isoch_list[m_i][a_i],
-        [-1., -1., e, d, mass, binar_f], e_max, bin_mass_ratio, cmd_sel)
+        e_max, bin_mr, err_lst, completeness, st_dist_mass, isochrone,
+        ext_coefs, N_fc, synth_cl_params)
 
     return shift_isoch, synth_clst
