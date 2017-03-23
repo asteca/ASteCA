758547057288b1b48496741bc4021e91403eec2a
Gabriel-p <gabrielperren@gmail.com>
2017-03-23 18:35:58 -0300

    fast general tolstoy, better dolphin, add mighell (testing)

--- packages/best_fit/likelihood.py
+++ packages/best_fit/likelihood.py
@@ -2,7 +2,7 @@
 import numpy as np
 from ..synth_clust import synth_cluster
 
-#############################################################
+# ############################################################
 # # Timer function: http://stackoverflow.com/a/21860100/1391441
 # from contextlib import contextmanager
 # import time
@@ -16,15 +16,33 @@ from ..synth_clust import synth_cluster
 #     finally:
 #         end = time.clock()
 #         print ('{} elapsed: {}'.format(label, end - start))
-#############################################################
+# ############################################################
 
 
 def tolstoy(synth_clust, obs_clust):
     '''
-    Takes a synthetic cluster, compares it to the observed cluster and
+    Compares the observed cluster with a synthetic cluster,
     returns the weighted (log) likelihood value.
     This function follows the recipe given in Tolstoy & Saha (1996),
     Hernandez & Valls-Gabaud (2008) and Monteiro, Dias & Caetano (2010).
+
+    Likelihood form:
+
+    \begin{equation}
+    -\ln L = N\ln M -
+             \sum\limits_{i=1}^{N} \ln
+             \left\{
+                P_i \sum\limits_{j=1}^M \frac{\exp
+                    \left[
+                        -\frac{1}{2}
+                            \left(
+                                \sum_{k=1}^D \frac{(f_{ik}-g_{jk})^2}
+                                {\sigma_{ik}^2 + \rho_{jk}^2}
+                            \right)
+                    \right]}
+                {\prod_{k=1}^D \sqrt{\sigma_{ik}^2 + \rho_{jk}^2}}
+            \right\}
+    \end{equation}
     '''
 
     # If synthetic cluster is empty, assign high likelihood value.
@@ -43,49 +61,29 @@ def tolstoy(synth_clust, obs_clust):
         for st_phot, st_e_phot in zip(zip(*synth_phot), zip(*synth_errors)):
             syn_st.append(zip(*[st_phot, st_e_phot]))
 
-        # Probability for each observed star.
-        cl_stars_probs = []
-        # For each observed star.
-        for o_st in obs_st:
-            # o_st = [phot_1, phot_2, phot_3, ...]
-            # o_phot_j = [phot_val, error]
+        # Photometric difference (observed - synthetic), for all dimensions.
+        phot_dif = np.array(obs_st)[:, None, :, 0] -\
+            np.array(syn_st)[None, :, :, 0]
+        # Sum of squared photometric errors, for all dimensions.
+        sigma_sum = np.array(obs_st)[:, None, :, 1] +\
+            np.array(syn_st)[None, :, :, 1]
 
-            # Summatory over all synthetic stars.
-            syn_sum = 0.
-            # For each synthetic star.
-            for s_st in syn_st:
-                # s_st = [phot_1, phot_2, phot_3, ...]
-                # s_phot_j = [phot_val, error]
+        # Sum for all photometric dimensions.
+        Dsum = (np.square(phot_dif) / sigma_sum).sum(axis=-1)
+        # Product of summed squared sigmas.
+        sigma_prod = np.prod(sigma_sum, axis=-1)
+        # All elements inside synthetic stars summatory.
+        sum_M_j = np.exp(-0.5 * Dsum) / np.sqrt(sigma_prod)
+        # Sum for all synthetic stars.
+        sum_M = np.sum(sum_M_j, axis=-1)
 
-                # Summatory for each photometric dimension.
-                exp_sum, err_mult = 0., 1.
-                # For each photometric dimension stored.
-                for k, o_p in enumerate(o_st):
-                    # Photometric difference squared, divided by the sum of
-                    # the squared photometric errors, for this observed star,
-                    # compared to this synthetic star, in this photometric
-                    # dimension.
-                    exp_sum += np.square(o_p[0] - s_st[k][0]) /\
-                        (o_p[1] + s_st[k][1])
-                    # Multiplication of square root of the sum of the squared
-                    # photometric errors.
-                    err_mult = err_mult * (o_p[1] + s_st[k][1])
+        # Multiply by membership probabilities.
+        sum_M_MP = mem_probs * sum_M
+        # Remove 0. elements before applying the logarithm below.
+        sum_N = sum_M_MP[sum_M_MP != 0.]
 
-                # Probability that the observed star 'o_st' is a star from
-                # this synthetic model.
-                syn_sum += np.exp(-0.5 * exp_sum) / np.sqrt(err_mult)
-
-            # The final prob for this observed star is the sum over all
-            # synthetic stars.
-            cl_stars_probs.append(syn_sum)
-
-        # Weight the probability of each observed star by its MP.
-        # Use 1e-10 to avoid inf values in the np.log that follows.
-        cl_stars_probs_w = cl_stars_probs * mem_probs
-        cl_stars_probs_w[cl_stars_probs_w == 0.] = 1.e-10
-        # Normalize by the number of synthetic stars, and sum the (negative)
-        # logarithms to obtain the final likelihood.
-        tlst_lkl = -sum(np.log(cl_stars_probs_w / len(syn_st)))
+        # Final negative logarithmic likelihood
+        tlst_lkl = len(obs_st) * np.log(len(syn_st)) - sum(np.log(sum_N))
 
     return tlst_lkl
 
@@ -100,73 +98,59 @@ def dolphin(synth_clust, obs_clust):
     # If synthetic cluster is empty, assign high likelihood value.
     if not synth_clust:
         # If synthetic cluster is empty, assign a large likelihood value.
-        dolph_lkl = 1e09
+        dolph_lkl = 1.e09
     else:
-        # with timeblock("  Histodd"):
         synth_phot = synth_clust[0][0]
         # Observed cluster's histogram and bin edges for each dimension.
-        cl_histo, bin_edges = obs_clust
+        cl_histo_f, cl_z_idx, bin_edges = obs_clust[:3]
 
-        # with timeblock("  log(lkl)"):
         # Histogram of the synthetic cluster, using the bin edges calculated
         # with the observed cluster.
         syn_histo = np.histogramdd(synth_phot, bins=bin_edges)[0]
-        # Flatten N-dimensional histograms.
-        cl_histo_f = np.array(cl_histo).flatten()
-        syn_histo_f = np.array(syn_histo).flatten()
+        # Flatten N-dimensional histogram.
+        syn_histo_f = np.array(syn_histo).ravel()
+        # Remove all bins where n_i = 0 (no observed stars).
+        syn_histo_f = syn_histo_f[cl_z_idx]
+
         # Assign small value to the zero elements in 'syn_histo_f'.
-        syn_histo_f[syn_histo_f == 0] = 1.e-9
+        # The value equals 1 star divided among all empty bins.
+        syn_histo_f[syn_histo_f == 0] =\
+            1. / max(np.count_nonzero(syn_histo_f == 0), 1.)
 
         # Obtain inverse logarithmic 'Poisson likelihood ratio'.
-        dolph_lkl = 2. * (
-            len(synth_phot[0]) - sum(cl_histo_f * np.log(syn_histo_f)))
+        dolph_lkl = synth_phot[0].size -\
+            np.sum(cl_histo_f * np.log(syn_histo_f))
 
     return dolph_lkl
 
 
-def mighell(Q, P):
+def mighell(synth_clust, obs_clust):
     '''
     '''
-
-    if not Q.any():
-        chi = 10000.
+    if not synth_clust:
+        mig_chi = 1.e09
     else:
+        # Observed cluster's histogram and bin edges for each dimension.
+        cl_histo_f, cl_z_idx, bin_edges = obs_clust[:3]
 
-        # Observed cluster's histogram.
-        cl_histo = P[0]
-        # Bin edges for each dimension.
-        b_rx, b_ry = P[1]
-
-        # Magnitude and color for the synthetic cluster.
-        syn_mags_cols = np.array(zip(*[Q[0], Q[2]]))
+        # Synthetic cluster.
+        synth_phot = synth_clust[0][0]
         # Histogram of the synthetic cluster, using the bin edges calculated
         # with the observed cluster.
-        syn_histo = np.histogramdd(syn_mags_cols, bins=[b_rx, b_ry])[0]
+        syn_histo = np.histogramdd(synth_phot, bins=bin_edges)[0]
 
-        chi = 0.
-        for el1 in zip(*(cl_histo, syn_histo)):
-            for el2 in zip(*(el1[0], el1[1])):
-                c = np.square(el2[0] + min(el2[0], 1) - el2[1]) / (el2[0] + 1)
-                chi += c
+        # Flatten N-dimensional histogram.
+        syn_histo_f = np.array(syn_histo).ravel()
+        # Remove bins that are empty in both arrays.
+        z = (cl_histo_f != 0) | (syn_histo_f != 0)
+        cl_histo_f, syn_histo_f = cl_histo_f[z], syn_histo_f[z]
 
-        # print len(Q[0]), chi
-        # import matplotlib.pyplot as plt
-        # fig = plt.figure()
-        # ax1 = fig.add_subplot(1, 3, 1)
-        # ax2 = fig.add_subplot(1, 3, 2)
-        # ax3 = fig.add_subplot(1, 3, 3)
-        # ax1.imshow(d_1.transpose(), origin='lower', aspect='auto')
-        # ax2.imshow(d_2.transpose(), origin='lower', aspect='auto')
-        # ax3.scatter(P[4], P[2], c='r')
-        # ax3.scatter(Q[0], Q[2], c='b')
-        # text1 = 'chi = {:.2f}\n'.format(chi)
-        # text2 = 'N = {}'.format(len(Q[0]))
-        # text = text1 + text2
-        # ax3.text(0.05, 0.9, text, transform=ax3.transAxes)
-        # fig.subplots_adjust(hspace=1)
-        # plt.show()
+        # Final chi.
+        mig_chi = np.sum(np.square(
+            cl_histo_f + np.clip(cl_histo_f, 0, 1) - syn_histo_f) /
+            (cl_histo_f + 1.))
 
-    return chi
+    return mig_chi
 
 
 def main(lkl_method, e_max, bin_mass_ratio, err_lst, obs_clust,
@@ -192,8 +176,9 @@ def main(lkl_method, e_max, bin_mass_ratio, err_lst, obs_clust,
     # Obtain the likelihood matching the synthetic and observed clusters.
     if lkl_method == 'tolstoy':
         likelihood = tolstoy(synth_clust, obs_clust)
-    else:
+    elif lkl_method == 'dolphin':
         likelihood = dolphin(synth_clust, obs_clust)
-        # likelihood = mighell(synth_clust, obs_clust)
+    elif lkl_method == 'mighell':
+        likelihood = mighell(synth_clust, obs_clust)
 
     return likelihood
