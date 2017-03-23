7657e5bba590395566e29a33a93bae3b662a0f2b
Gabriel-p <gabrielperren@gmail.com>
2017-03-23 12:15:34 -0300

    close #100

--- packages/out/make_D1_plot.py
+++ packages/out/make_D1_plot.py
@@ -15,26 +15,31 @@ def main(
 
     # flag_make_plot = pd['pl_params'][0]
     if pd['pl_params'][0]:
-        fig = plt.figure(figsize=(30, 25))
-        gs = gridspec.GridSpec(10, 12)
-        add_version_plot.main(.94)
 
         best_fit_algor, lkl_method, bin_method, N_b = pd['bf_params']
 
         # Best fit plots.
         if pd['bf_flag']:
+            fig = plt.figure(figsize=(30, 25))
+            gs = gridspec.GridSpec(10, 12)
+            if best_fit_algor == 'genet':
+                add_version_plot.main()
+            else:
+                add_version_plot.main(y_fix=.8)
+
+            min_max_p = prep_plots.param_ranges(pd['fundam_params'])
+            # Get special axis ticks for metallicity.
+            xp_min, xp_max = min_max_p[0]
+            # The max number of characters in the axis '30', is HARD-CODED.
+            # Add values to the end of this list.
+            min_max_p.append(prep_plots.BestTick(xp_min, xp_max, 30))
+
+            model_done = isoch_fit_params[-1]
+
             # Best fitting process plots for GA.
             if best_fit_algor == 'genet':
 
-                min_max_p = prep_plots.param_ranges(pd['fundam_params'])
-                # Get special axis ticks for metallicity.
-                xp_min, xp_max = min_max_p[0]
-                # The max number of characters in the axis '30', is HARD-CODED.
-                # Add values to the end of this list.
-                min_max_p.append(prep_plots.BestTick(xp_min, xp_max, 30))
-
-                # Unpack.
-                lkl_old, new_bs_indx, model_done = isoch_fit_params[1:4]
+                lkl_old, new_bs_indx = isoch_fit_params[1:3]
                 l_min_max = prep_plots.likl_y_range(lkl_old)
 
                 arglist = [
@@ -49,32 +54,37 @@ def main(
                     [gs, 'metal-dist', min_max_p, isoch_fit_params,
                      isoch_fit_errors, model_done],
                     [gs, 'mass-binar', min_max_p, isoch_fit_params,
-                     isoch_fit_errors, model_done],
-                    # pl_lkl_scatt: Parameter likelihood density plot.
-                    [gs, '$z$', min_max_p, isoch_fit_params, isoch_fit_errors,
-                        model_done],
-                    [gs, '$log(age)$', min_max_p, isoch_fit_params,
-                     isoch_fit_errors, model_done],
-                    [gs, '$E_{{(B-V)}}$', min_max_p, isoch_fit_params,
-                        isoch_fit_errors, model_done],
-                    [gs, '$(m-M)_o$', min_max_p, isoch_fit_params,
-                     isoch_fit_errors, model_done],
-                    [gs, '$M\,(M_{{\odot}})$', min_max_p, isoch_fit_params,
-                        isoch_fit_errors, model_done],
-                    [gs, '$b_{{frac}}$', min_max_p, isoch_fit_params,
                      isoch_fit_errors, model_done]
                 ]
                 for n, args in enumerate(arglist):
                     mp_best_fit1.plot(n, *args)
 
-        # Generate output file.
-        fig.tight_layout()
-        pl_fmt, pl_dpi = pd['pl_params'][1:3]
-        plt.savefig(
-            join(npd['output_subdir'], str(npd['clust_name']) +
-                 '_D1.' + pl_fmt), dpi=pl_dpi, bbox_inches='tight')
-        # Close to release memory.
-        plt.clf()
-        plt.close()
+            arglist = [
+                # pl_lkl_scatt: Parameter likelihood density plot.
+                [gs, '$z$', min_max_p, isoch_fit_params, isoch_fit_errors,
+                    model_done],
+                [gs, '$log(age)$', min_max_p, isoch_fit_params,
+                 isoch_fit_errors, model_done],
+                [gs, '$E_{{(B-V)}}$', min_max_p, isoch_fit_params,
+                    isoch_fit_errors, model_done],
+                [gs, '$(m-M)_o$', min_max_p, isoch_fit_params,
+                 isoch_fit_errors, model_done],
+                [gs, '$M\,(M_{{\odot}})$', min_max_p, isoch_fit_params,
+                    isoch_fit_errors, model_done],
+                [gs, '$b_{{frac}}$', min_max_p, isoch_fit_params,
+                 isoch_fit_errors, model_done]
+            ]
+            for n, args in enumerate(arglist):
+                mp_best_fit1.plot(n + 5, *args)
 
-        print("<<Plots from 'D1' block created>>")
+            # Generate output file.
+            fig.tight_layout()
+            pl_fmt, pl_dpi = pd['pl_params'][1:3]
+            plt.savefig(
+                join(npd['output_subdir'], str(npd['clust_name']) +
+                     '_D1.' + pl_fmt), dpi=pl_dpi, bbox_inches='tight')
+            # Close to release memory.
+            plt.clf()
+            plt.close()
+
+            print("<<Plots from 'D1' block created>>")
