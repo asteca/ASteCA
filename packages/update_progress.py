65d1f89bd0992120c8401c80ef976ba3c3803c38
Gabriel-p <gabrielperren@gmail.com>
2017-08-24 16:11:26 -0300

    fix bug in progress bar

--- packages/update_progress.py
+++ packages/update_progress.py
@@ -9,11 +9,12 @@ def updt(total, progress):
     Original source: https://stackoverflow.com/a/15860757/1391441
     """
     barLength, status = 20, ""
-    progress = round(float(progress) / float(total), 1)
+    progress = float(progress) / float(total)
     if progress >= 1.:
         progress, status = 1, "\r\n"
     block = int(round(barLength * progress))
-    text = "\r[{0}] {1}% {2}".format(
-        "#" * block + "-" * (barLength - block), progress * 100, status)
+    text = "\r[{}] {:.0f}% {}".format(
+        "#" * block + "-" * (barLength - block), round(progress * 100, 0),
+        status)
     sys.stdout.write(text)
     sys.stdout.flush()
