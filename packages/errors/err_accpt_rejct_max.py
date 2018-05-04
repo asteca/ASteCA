be82da8a566a9c31802ead69fe66efbac844b4c3
Gabriel-p <gabrielperren@gmail.com>
2018-05-04 11:53:13 -0500

    ignore nan related warning

--- packages/errors/err_accpt_rejct_max.py
+++ packages/errors/err_accpt_rejct_max.py
@@ -1,14 +1,18 @@
 
 import numpy as np
+import warnings
 
 
 def main(cld, err_max):
     """
     Accept stars with photometric errors < e_max in mag and color.
     """
-    acpt_indx = np.flatnonzero(
-        ((cld['em'] < err_max).all(0) & (cld['ec'] < err_max).all(0))).tolist()
-    rjct_indx = np.setdiff1d(
-        np.arange(len(cld['em'][0])), acpt_indx).tolist()
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", RuntimeWarning)
+        acpt_indx = np.flatnonzero(
+            ((cld['em'] < err_max).all(0) &
+                (cld['ec'] < err_max).all(0))).tolist()
+        rjct_indx = np.setdiff1d(
+            np.arange(len(cld['em'][0])), acpt_indx).tolist()
 
     return acpt_indx, rjct_indx
