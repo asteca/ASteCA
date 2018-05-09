ff6f2947bd70e840b437f468fd58b8a2fecf7d8e
Gabriel-p <gabrielperren@gmail.com>
2018-05-09 10:50:08 -0500

    minor

--- packages/phot_analysis/members_number.py
+++ packages/phot_analysis/members_number.py
@@ -3,27 +3,21 @@
 def main(clp):
     """
     Calculate the approximate number of cluster's members inside the cluster's
-    radius. The process is as follows: field_dens is the density of field
-    stars per unit area so multiplying that by the cluster's area we get the
-    approx number of field stars inside the cluster. We can count
-    the total number of stars inside the radius, n_t and then
-    obtain n_c (which is the approx number of members) with:
+    radius as follows:
 
     n_c = n_clust - [field_dens * cluster_area]
     """
-
-    n_clust, cl_area, field_dens, clust_rad, rdp_length = clp['n_clust'],\
-        clp['cl_area'], clp['field_dens'], clp['clust_rad'], clp['rdp_length']
 
     # If the cluster radius exceeds the length of the area where the field
     # density value was obtained (ie: the extension of the RDP), then do not
     # obtain the n_memb parameter since the field density does not represent
     # the density of the field but rather the density of the outermost regions
     # of the cluster.
-    if clust_rad < rdp_length / 2.:
+    if clp['clust_rad'] < clp['rdp_length'] / 2.:
 
         # Approx number of members.
-        n_memb = max(int(round(n_clust - (field_dens * cl_area))), 0)
+        n_memb = max(int(round(
+            clp['n_clust'] - (clp['field_dens'] * clp['cl_area']))), 0)
 
         # Raise a flag if the number of members is < 10
         flag_num_memb_low = False if n_memb >= 10 else True
