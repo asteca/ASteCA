fd11cd09fcbeb921ef9238e1b81677616f414cd4
Gabriel-p <gabrielperren@gmail.com>
2018-05-09 10:45:06 -0500

    read both complete and incomplete data

--- packages/inp/get_data.py
+++ packages/inp/get_data.py
@@ -2,6 +2,7 @@
 import numpy as np
 from astropy.io import ascii
 from collections import defaultdict
+import operator
 
 
 def list_duplicates(seq):
@@ -34,28 +35,11 @@ def fill_cols(tbl, fill=np.nan, kind='f'):
             pass
 
 
-def main(npd, id_indx, x_indx, y_indx, mag_indx, e_mag_indx, col_indx,
-         e_col_indx, **kwargs):
-    '''
-    Read all data from the cluster's data file.
-    '''
-
-    data_file = npd['data_file']
-    try:
-        # Name of IDs column (the '+ 1' is because astropy Tables' first column
-        # is named 'col1', not 'col0'). Store IDs as strings.
-        id_colname = 'col' + str(id_indx + 1)
-        data = ascii.read(
-            data_file, fill_values=[('INDEF', '0'), ('9999.99', '0')],
-            converters={id_colname: [ascii.convert_numpy(np.str)]})
-        # Change masked elements with 'nan' values, in place.
-        fill_cols(data)
-
-    except ascii.InconsistentTableError:
-        raise ValueError("ERROR: could not read data input file:\n  {}\n"
-                         "  Check that all rows are filled (i.e., no blank"
-                         " spaces)\n  for all columns.\n".format(data_file))
-
+def dataCols(data_file, data, id_indx, x_indx, y_indx, mag_indx, e_mag_indx,
+             col_indx, e_col_indx):
+    """
+    Separate data into appropriate columns.
+    """
     try:
         # Read coordinates data.
         x, y = np.array(data.columns[x_indx]), np.array(data.columns[y_indx])
@@ -102,8 +86,42 @@ def main(npd, id_indx, x_indx, y_indx, mag_indx, e_mag_indx, col_indx,
                     "ERROR: the range for {} column {} is\nzero."
                     " Check the input data format.".format(data_names[i], i))
 
-    N_ids = ids.size
-    m_size, c_size = [], []
+    return ids, x, y, mags, cols, em, ec
+
+
+def main(npd, id_indx, x_indx, y_indx, mag_indx, e_mag_indx, col_indx,
+         e_col_indx, **kwargs):
+    '''
+    Read all data from the cluster's data file.
+    '''
+
+    data_file = npd['data_file']
+    try:
+        # Name of IDs column (the '+ 1' is because astropy Tables' first column
+        # is named 'col1', not 'col0'). Store IDs as strings.
+        id_colname = 'col' + str(id_indx + 1)
+        data = ascii.read(
+            data_file, fill_values=[('INDEF', '0'), ('9999.99', '0')],
+            converters={id_colname: [ascii.convert_numpy(np.str)]})
+
+        # Remove all rows with at least one masked element.
+        data_compl = data[reduce(
+            operator.and_, [~data[col].mask for col in data.columns])]
+
+        # Change masked elements with 'nan' values, in place.
+        fill_cols(data)
+
+    except ascii.InconsistentTableError:
+        raise ValueError("ERROR: could not read data input file:\n  {}\n"
+                         "  Check that all rows are filled (i.e., no blank"
+                         " spaces)\n  for all columns.\n".format(data_file))
+
+    # Create cluster's dictionary with the *incomplete* data.
+    ids, x, y, mags, cols, em, ec = dataCols(
+        data_file, data, id_indx, x_indx, y_indx, mag_indx, e_mag_indx,
+        col_indx, e_col_indx)
+    # Check percentage of complete data.
+    N_ids, m_size, c_size = ids.size, [], []
     for m in mags:
         m_size.append(np.count_nonzero(~np.isnan(m)))
     for c in cols:
@@ -114,8 +132,14 @@ def main(npd, id_indx, x_indx, y_indx, mag_indx, e_mag_indx, col_indx,
     if frac_reject > 0.05:
         print("  WARNING: {:.0f}% of stars in input file contain\n"
               "  invalid photometric data.".format(100. * frac_reject))
+    cld_i = {'ids': ids, 'x': x, 'y': y, 'mags': mags, 'em': em,
+             'cols': cols, 'ec': ec}
 
-    # Create cluster's data dictionary.
-    cld = {'ids': ids, 'x': x, 'y': y, 'mags': mags, 'em': em,
-           'cols': cols, 'ec': ec}
-    return cld
+    # Create cluster's dictionary with the *complete* data.
+    ids, x, y, mags, cols, em, ec = dataCols(
+        data_file, data_compl, id_indx, x_indx, y_indx, mag_indx, e_mag_indx,
+        col_indx, e_col_indx)
+    cld_c = {'ids': ids, 'x': x, 'y': y, 'mags': mags, 'em': em,
+             'cols': cols, 'ec': ec}
+
+    return cld_i, cld_c
