e473fd5dcff4f42a6d15a23f14eb6ee5fc5137f6
Gabriel-p <gabrielperren@gmail.com>
2018-05-10 16:01:17 -0500

    fix members_N_compare, how long was this broken?

--- packages/decont_algors/members_N_compare.py
+++ packages/decont_algors/members_N_compare.py
@@ -10,7 +10,7 @@ def main(clp):
     '''
     memb_par, n_memb_da, flag_memb_par = float("inf"), -1., False
     # Obtain parameter if the DA was applied.
-    if not clp['flag_decont_skip']:
+    if not clp['flag_decont_skip'] and clp['n_memb'] > 0:
 
         n_memb_da = 0
         # Number of stars assigned a MP>=0.5.
@@ -18,21 +18,18 @@ def main(clp):
             if star[7] >= 0.5:
                 n_memb_da += 1
 
-        # Obtain only if n_memb was calculated.
-        if not clp['flag_num_memb_low']:
+        # Obtain parameter.
+        memb_par = (float(clp['n_memb']) - float(n_memb_da)) / \
+            (float(clp['n_memb']) + float(n_memb_da))
 
-            if clp['n_memb'] != 0 or n_memb_da != 0:
-                # Obtain parameter.
-                memb_par = (float(clp['n_memb']) - float(n_memb_da)) / \
-                    (float(clp['n_memb']) + float(n_memb_da))
-            else:
-                print("  WARNING: could not obtain 'memb_par' parameter.")
+        # Set flag.
+        if abs(memb_par) > 0.33:
+            flag_memb_par = True
+            print("  WARNING: structural vs. photometric true cluster\n"
+                  "  members estimated differ by a factor > 2.")
 
-            # Set flag.
-            if abs(memb_par) > 0.33:
-                flag_memb_par = True
-                print("  WARNING: structural vs. photometric true cluster\n"
-                      "  members estimated differ by a factor > 2.")
+    else:
+        print("  WARNING: could not obtain 'memb_par' parameter.")
 
     clp['memb_par'], clp['n_memb_da'], clp['flag_memb_par'] =\
         memb_par, n_memb_da, flag_memb_par
