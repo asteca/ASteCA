7028e87139dc6373b9d008e3a363f73ce9a7d364
Gabriel-p <gabrielperren@gmail.com>
2018-10-10 15:53:34 -0300

    use tuple to remove warning

--- packages/best_fit/ptemcee/util.py
+++ packages/best_fit/ptemcee/util.py
@@ -44,9 +44,9 @@ def autocorr_function(x, axis=0, fast=False):
     # Compute the FFT and then (from that) the auto-correlation function.
     f = np.fft.fft(x - np.mean(x, axis=axis), n=2 * n, axis=axis)
     m[axis] = slice(0, n)
-    acf = np.fft.ifft(f * np.conjugate(f), axis=axis)[m].real
+    acf = np.fft.ifft(f * np.conjugate(f), axis=axis)[tuple(m)].real
     m[axis] = 0
-    return acf / acf[m]
+    return acf / acf[tuple(m)]
 
 
 def autocorr_integrated_time(x, axis=0, window=50, fast=False):
@@ -82,7 +82,7 @@ def autocorr_integrated_time(x, axis=0, window=50, fast=False):
     # N-dimensional case.
     m = [slice(None), ] * len(f.shape)
     m[axis] = slice(1, window)
-    tau = 1 + 2 * np.sum(f[m], axis=axis)
+    tau = 1 + 2 * np.sum(f[tuple(m)], axis=axis)
 
     return tau
 
