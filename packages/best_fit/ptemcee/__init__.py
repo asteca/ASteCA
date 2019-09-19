86d4bb196215a0bae0d12f50b67c88f13dc07065
Gabriel <gabrielperren@gmail.com>
2019-09-19 17:44:21 -0300

    not used by ptemcee

--- packages/best_fit/ptemcee/__init__.py
+++ packages/best_fit/ptemcee/__init__.py
@@ -4,8 +4,8 @@
 from __future__ import (division, print_function, absolute_import, unicode_literals)
 
 from .sampler import *
-from .interruptible_pool import InterruptiblePool
-from .mpi_pool import MPIPool
+# from .interruptible_pool import InterruptiblePool
+# from .mpi_pool import MPIPool
 from . import util
 
 __version__ = '1.0.0'
