1f84ceb11d39f6e3a779ecbc2b93677c1483424b
Gabriel <gabrielperren@gmail.com>
2019-11-25 09:40:03 -0300

    radius and field density functions changes, reflect changes here

--- packages/check/params_struct.py
+++ packages/check/params_struct.py
@@ -3,7 +3,7 @@ import sys
 from collections import Counter
 
 
-def check(manual_struct, center_bw, radius_method, fdens_method, **kwargs):
+def check(manual_struct, center_bw, fdens_method, **kwargs):
     """
     Check that the parameters are properly written.
     """
@@ -20,17 +20,12 @@ def check(manual_struct, center_bw, radius_method, fdens_method, **kwargs):
                  "than (or equal to) zero.".format(center_bw))
 
     # Radius finding function.
-    if radius_method not in ('low', 'mid', 'high'):
-        sys.exit("ERROR: mode selected ('{}') for radius finding"
-                 " function is not valid.".format(radius_method))
-
-    # Radius finding function.
     try:
         fd = float(fdens_method)
         if fd < 0.:
             sys.exit("ERROR: field density ('{}') must be"
                      " greater than zero.".format(fd))
     except ValueError:
-        if fdens_method != 'auto':
+        if fdens_method not in ('min', 'last'):
             sys.exit("ERROR: field density mode ('{}') is not"
                      " recognized.".format(fdens_method))
