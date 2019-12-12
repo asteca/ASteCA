47edc463ad2477546a81a5babfedbd600168f0ac
Gabriel <gabrielperren@gmail.com>
2019-12-12 11:56:00 -0300

    dont use dummy binary data

--- packages/inp/interp_isochs.py
+++ packages/inp/interp_isochs.py
@@ -19,13 +19,19 @@ def main(
     mX = [age1, age2, ..., ageM]
     ageX = [f1,.., c1, c2,.., f1b,.., c1b, c2b,.., bp, mb, m_ini,.., m_bol]
     where:
-    fX:  individual filters (mags)
+    fX:  individual filters (mags). Currently a single main mag is allowed.
     cX:  colors
-    fXb: filters with binary data
-    cXb: colors with the binary data
-    bp:  binary probabilities
-    mb:  binary masses
+    fXb: filters with binary data  ------
+    cXb: colors with the binary data    | Only stored if binarity is set as a
+    bp:  binary probabilities           | free param, or fixed to a value > 0.
+    mb:  binary masses  -----------------
     Mini,...: extra parameters.
+
+    theor_tracks.shape = (Nz, Na, Nd, Ni)
+    Nz: number of metallicities
+    Na: number of log(age)s
+    Nd: number of data columns
+    Ni: number of interpolated values
 
     """
 
@@ -35,25 +41,32 @@ def main(
     mags_intp, cols_intp, mags_cols_intp, extra_pars_intp = interp_isoch_data(
         (mags_theor, cols_theor, mags_cols_theor, extra_pars), N_mass_interp)
 
-    # # Size of arrays in memory
-    # sz = 0.
-    # for arr in [mags_intp, cols_intp, mags_cols_intp, extra_pars_intp]:
-    #     sz += np.array(arr).size * np.array(arr).itemsize
-    # print("{:.3f} Mbs".format(sz / (1024.**2)))
-
     # The magnitudes for each defined color ('mags_cols_intp') are used here
     # and discarded after the colors (and magnitudes) with binarity assignment
     # are obtained.
-    mags_binar, cols_binar, probs_binar, mass_binar = binarity.binarGen(
+    binar_data = binarity.binarGen(
         binar_fracs, N_mass_interp, mags_intp, cols_intp, mags_cols_intp,
         extra_pars_intp, bin_mr)
 
     # Combine all data into a single array of shape:
     # (N_z, N_age, N_data, N_IMF_interp), where 'N_data' is the number of
     # sub-arrays in each array.
-    theor_tracks = np.concatenate(
-        (mags_intp, cols_intp, mags_binar, cols_binar, probs_binar, mass_binar,
-            extra_pars_intp), axis=2)
+    if binar_data is None:
+        theor_tracks = np.concatenate((
+            mags_intp, cols_intp, extra_pars_intp), axis=2)
+        # Index of m_ini (theoretical initial mass), stored in the theoretical
+        # isochrones.
+        m_ini_idx = np.shape(mags_intp)[2] + np.shape(cols_intp)[2]
+        binar_flag = False
+    else:
+        mags_binar, cols_binar, probs_binar, mass_binar = binar_data
+        theor_tracks = np.concatenate(
+            (mags_intp, cols_intp, mags_binar, cols_binar, probs_binar,
+                mass_binar, extra_pars_intp), axis=2)
+        # Two columns per mag and color, plus two extra columns: binary
+        # probability and mass.
+        m_ini_idx = 2 * (np.shape(mags_intp)[2] + np.shape(cols_intp)[2]) + 2
+        binar_flag = True
 
     # DEPRECATED 02-10-2019
     #
@@ -95,7 +108,7 @@ def main(
     #     filename, dtype='float64', mode='w+', shape=sp)
     # theor_tracks_mp[:] = theor_tracks[:]
 
-    return theor_tracks
+    return theor_tracks, m_ini_idx, binar_flag
 
 
 def interpPoints(mags_theor):
