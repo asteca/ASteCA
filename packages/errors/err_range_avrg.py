68c0a29609f79aa680539ca26cd389fc722ceaab
Gabriel <gabrielperren@gmail.com>
2020-02-19 12:22:11 -0300

    minor re-arrange

--- packages/errors/err_range_avrg.py
+++ packages/errors/err_range_avrg.py
@@ -5,6 +5,36 @@ from ..errors import err_medians
 from ..math_f import exp_function
 
 
+def main(clp):
+    '''
+    Fit an exponential function to the errors in each photometric dimension,
+    using the main magnitude as the x coordinate.
+    This data is used to display the error bars, and more importantly, to
+    generate the synthetic clusters in the best match module.
+    '''
+    # Use the main magnitude after max error rejection.
+    mmag = np.array(list(zip(*(list(zip(*clp['acpt_stars_c']))[3])))[0])
+    be_m, interv_mag, n_interv, mmag_interv_pts = errorData(mmag)
+
+    # Obtain the median points for photometric errors. Append magnitude
+    # values first, and colors after.
+    e_mags = list(zip(*(list(zip(*clp['acpt_stars_c']))[4])))
+    e_cols = list(zip(*(list(zip(*clp['acpt_stars_c']))[6])))
+    e_mc_medians = []
+    for e_mc in e_mags + e_cols:
+        e_mc_medians.append(err_medians.main(
+            mmag, e_mc, be_m, interv_mag, n_interv))
+
+    # Fit exponential curve for each photometric error dimension.
+    err_lst = []
+    for e_mc_v in e_mc_medians:
+        popt_mc = get_m_c_errors(mmag, e_mc_v, mmag_interv_pts)
+        err_lst.append(popt_mc)
+
+    clp['err_lst'] = err_lst
+    return clp
+
+
 def errorData(mmag):
     """
     Use the main magnitude to determine the error parameters that will be
@@ -77,33 +107,3 @@ def get_m_c_errors(mags, e_mc_v, mmag_interv_pts):
             popt_mc = np.insert(popt_mc, 2, 0.)
 
     return popt_mc
-
-
-def main(clp):
-    '''
-    Fit an exponential function to the errors in each photometric dimension,
-    using the main magnitude as the x coordinate.
-    This data is used to display the error bars, and more importantly, to
-    generate the synthetic clusters in the best match module.
-    '''
-    # Use the main magnitude after max error rejection.
-    mmag = np.array(list(zip(*(list(zip(*clp['acpt_stars_c']))[3])))[0])
-    be_m, interv_mag, n_interv, mmag_interv_pts = errorData(mmag)
-
-    # Obtain the median points for photometric errors. Append magnitude
-    # values first, and colors after.
-    e_mags = list(zip(*(list(zip(*clp['acpt_stars_c']))[4])))
-    e_cols = list(zip(*(list(zip(*clp['acpt_stars_c']))[6])))
-    e_mc_medians = []
-    for e_mc in e_mags + e_cols:
-        e_mc_medians.append(err_medians.main(
-            mmag, e_mc, be_m, interv_mag, n_interv))
-
-    # Fit exponential curve for each photometric error dimension.
-    err_lst = []
-    for e_mc_v in e_mc_medians:
-        popt_mc = get_m_c_errors(mmag, e_mc_v, mmag_interv_pts)
-        err_lst.append(popt_mc)
-
-    clp['err_lst'] = err_lst
-    return clp
