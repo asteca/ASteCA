155b6146451175eee3a93dcfd6aae15ce86c57a5
Gabriel <gabrielperren@gmail.com>
2020-09-04 18:59:17 -0300

    handle outliers + fix small bug in nburn line + re-arrange

--- packages/out/BayesPlots.py
+++ packages/out/BayesPlots.py
@@ -6,6 +6,66 @@ from ..aux_funcs import reject_outliers
 from . cornerPlot import hist2d
 
 
+def autocorr(gs, gsx, gsy, Nsteps, tau_autocorr, ESS):
+    """
+    Mean autocorrelation time.
+    """
+    plt.subplot(gs[gsy[0]:gsy[1], gsx[0]:gsx[1]])
+    plt.plot(
+        Nsteps * np.arange(tau_autocorr.size), tau_autocorr,
+        label=r"$N_{{ESS}}\approx${:.0f}".format(ESS))
+    plt.xlabel("Steps")
+    plt.ylabel(r"$\hat{\tau}$")
+    plt.legend()
+
+
+def meanAF(gs, gsx, gsy, Nsteps, mean_afs):
+    """
+    Mean acceptance fraction.
+    """
+    plt.subplot(gs[gsy[0]:gsy[1], gsx[0]:gsx[1]])
+    plt.plot(Nsteps * np.arange(mean_afs.size), mean_afs,
+             label=r"$MAF\approx${:.2f}".format(mean_afs[-1]))
+    plt.xlabel("Steps")
+    plt.ylabel(r"$MAF$")
+    plt.legend()
+
+
+def traceplot(
+    gs, gsx, gsy, mcmc_samples, _16_50_84_mean_mode, Brn_prcnt, xylabel,
+        xticks=True, tp_p=.5):
+    """
+    Chains traceplot.
+
+    tp_p : percentage of the final portion of the trace that will be used to
+    set the limits in the y axis.
+    """
+    ax = plt.subplot(gs[gsy[0]:gsy[1], gsx[0]:gsx[1]])
+    N_tot = mcmc_samples.shape[0]
+    plt.plot(mcmc_samples, c='k', lw=.8, ls='-', alpha=0.5)
+    Nburn = Brn_prcnt * N_tot
+    plt.axvline(x=Nburn, linestyle=':', color='r', zorder=4)
+    # 16th and 84th percentiles + median.
+    plt.axhline(
+        y=_16_50_84_mean_mode[0], linestyle=':', color='orange', zorder=4)
+    plt.axhline(
+        y=_16_50_84_mean_mode[3], linestyle=':', color='blue', zorder=4)
+    plt.axhline(
+        y=_16_50_84_mean_mode[2], linestyle=':', color='orange', zorder=4)
+    if xticks is True:
+        plt.xlabel("Steps")
+    else:
+        plt.xticks([])
+    plt.ylabel(xylabel)
+    # Use the last X% of the chains.
+    N = int(mcmc_samples.shape[0] * tp_p)
+    std = np.std(mcmc_samples[-N:])
+    pmin, pmax = np.min(mcmc_samples[-N:]), np.max(mcmc_samples[-N:])
+    ymin, ymax = pmin - std, pmax + std
+    ax.set_ylim(ymin, ymax)
+    ax.set_xlim(0, N_tot)
+
+
 def histogram(
     gs, gsx, gsy, mcmc_samples, _16_50_84_mean_mode, mu_x_kde,
         xylabel, dec_places, xlims=None):
@@ -49,7 +109,7 @@ def histogram(
 
     if xlims is None:
         # MAD is robust to outliers
-        mad = 1.4826 * stats.median_absolute_deviation(mcmc_samples.flatten())
+        mad = 1.4826 * stats.median_absolute_deviation(no_outlr)
         plt.xlim(_50 - 4. * mad, _50 + 4. * mad)
     else:
         plt.xlim(xlims[0], xlims[1])
@@ -58,72 +118,17 @@ def histogram(
     plt.legend()
 
 
-def traceplot(
-    gs, gsx, gsy, mcmc_samples, _16_50_84_mean_mode, Brn_prcnt, xylabel,
-        xticks=True, tp_p=.5):
-    """
-    Chains traceplot.
-
-    tp_p : percentage of the final portion of the trace that will be used to
-    set the limits in the y axis.
-    """
-    ax = plt.subplot(gs[gsy[0]:gsy[1], gsx[0]:gsx[1]])
-    N_tot = mcmc_samples.shape[0]
-    plt.plot(mcmc_samples, c='k', lw=.8, ls='-', alpha=0.5)
-    Nburn = Brn_prcnt * N_tot
-    plt.axvline(x=Nburn, linestyle=':', color='r', zorder=4)
-    # 16th and 84th percentiles + median.
-    plt.axhline(
-        y=_16_50_84_mean_mode[0], linestyle=':', color='orange', zorder=4)
-    plt.axhline(
-        y=_16_50_84_mean_mode[3], linestyle=':', color='blue', zorder=4)
-    plt.axhline(
-        y=_16_50_84_mean_mode[2], linestyle=':', color='orange', zorder=4)
-    if xticks is True:
-        plt.xlabel("Steps")
-    else:
-        plt.xticks([])
-    plt.ylabel(xylabel)
-    # Use the last X% of the chains.
-    N = int(mcmc_samples.shape[0] * tp_p)
-    std = np.std(mcmc_samples[-N:])
-    pmin, pmax = np.min(mcmc_samples[-N:]), np.max(mcmc_samples[-N:])
-    ymin, ymax = pmin - std, pmax + std
-    ax.set_ylim(ymin, ymax)
-    ax.set_xlim(0, N_tot)
-
-
-def autocorr(gs, gsx, gsy, Nsteps, tau_autocorr, ESS):
-    """
-    Mean autocorrelation time.
-    """
-    plt.subplot(gs[gsy[0]:gsy[1], gsx[0]:gsx[1]])
-    plt.plot(
-        Nsteps * np.arange(tau_autocorr.size), tau_autocorr,
-        label=r"$N_{{ESS}}\approx${:.0f}".format(ESS))
-    plt.xlabel("Steps")
-    plt.ylabel(r"$\hat{\tau}$")
-    plt.legend()
-
-
-def meanAF(gs, gsx, gsy, Nsteps, mean_afs):
-    """
-    Mean acceptance fraction.
-    """
-    plt.subplot(gs[gsy[0]:gsy[1], gsx[0]:gsx[1]])
-    plt.plot(Nsteps * np.arange(mean_afs.size), mean_afs,
-             label=r"$MAF\approx${:.2f}".format(mean_afs[-1]))
-    plt.xlabel("Steps")
-    plt.ylabel(r"$MAF$")
-    plt.legend()
-
-
 def twoParDens(
         gs, gsx, gsy, x_samples, y_samples, KP_Bys_rc, KP_Bys_rt, xylabel):
     """
     """
     ax = plt.subplot(gs[gsy[0]:gsy[1], gsx[0]:gsx[1]])
-    hist2d(ax, x_samples, y_samples)
+
+    x_no_outlr = reject_outliers(x_samples.flatten())
+    y_no_outlr = reject_outliers(y_samples.flatten())
+    bin_edges = (np.linspace(x_no_outlr.min(), x_no_outlr.max(), 20),
+                 np.linspace(y_no_outlr.min(), y_no_outlr.max(), 20))
+    hist2d(ax, x_samples, y_samples, bin_edges)
     plt.scatter(
         KP_Bys_rc[1], KP_Bys_rt[1], marker='x', c='green', s=50, zorder=5)
     plt.scatter(
@@ -135,9 +140,12 @@ def twoParDens(
     plt.ylabel(xylabel[1])
 
     xl, yl = ax.get_xlim(), ax.get_ylim()
-    xmed, xstd = np.median(x_samples), np.std(x_samples)
-    ymed, ystd = np.median(y_samples), np.std(y_samples)
-    x3s_min, x3s_max = xmed - 3. * xstd, xmed + 3. * xstd
-    y3s_min, y3s_max = ymed - 3. * ystd, ymed + 3. * ystd
+    # MAD is robust to outliers
+    xmad = 1.4826 * stats.median_absolute_deviation(x_no_outlr)
+    ymad = 1.4826 * stats.median_absolute_deviation(y_no_outlr)
+    xsm, ysm = min(xmad, np.std(x_no_outlr)), min(ymad, np.std(y_no_outlr))
+    xmed, ymed = np.median(x_no_outlr), np.median(y_no_outlr)
+    x3s_min, x3s_max = xmed - 3. * xsm, xmed + 3. * xsm
+    y3s_min, y3s_max = ymed - 3. * ysm, ymed + 3. * ysm
     plt.xlim(max(xl[0], x3s_min), min(xl[1], x3s_max))
     plt.ylim(max(yl[0], y3s_min), min(yl[1], y3s_max))
