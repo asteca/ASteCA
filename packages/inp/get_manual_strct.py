bb0b7d04f998516ee8899c3054352f073782ca7a
Gabriel <gabrielperren@gmail.com>
2021-05-31 18:25:55 -0300

    take general structural parameters from 'CLUSTER'

--- packages/inp/get_manual_strct.py
+++ packages/inp/get_manual_strct.py
@@ -2,9 +2,11 @@
 
 def main(pd, clust_name, **kwargs):
     """
-    Read the range values for trimming the frame, if given.
+    Range values for trimming the frame, if given.
+
     Read center, field density, radius and number of field regions for this
-    cluster.
+    cluster. If this cluster has no values set for these parameters, use the
+    general values from the 'CLUSTER' entry.
     """
 
     pd['flag_tf'], pd['tf_range'] = False, [0., 0., 0., 0.]
@@ -12,17 +14,22 @@ def main(pd, clust_name, **kwargs):
         if line[0] == clust_name:
             pd['flag_tf'], pd['tf_range'] = True, line[1]
 
-    # Default values
-    pd['cent_method'], pd['fdens_method'], pd['rad_method'],\
-        pd['fregs_method'] = ('a', 'a'), 'a', 'a', 'a'
-
+    clFlag = False
     try:
         cx, cy, fdens, rad, fregs = pd['manual_struct'][clust_name]
         pd['cent_method'] = (cx, cy)
         pd['fdens_method'] = fdens
         pd['rad_method'] = rad
         pd['fregs_method'] = fregs
+        clFlag = True
     except KeyError:
         pass
+
+    # Default values
+    if clFlag is False:
+        if 'CLUSTER' in pd['manual_struct'].keys():
+            cx, cy, fdens, rad, fregs = pd['manual_struct']['CLUSTER']
+        else:
+            raise ValueError("Line 'S0' in 'asteca.ini' missing 'CLUSTER'")
 
     return pd
