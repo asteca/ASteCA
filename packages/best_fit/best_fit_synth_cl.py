4b32f724cc015f22058d8de228c4bf1e1aed5b3d
Gabriel <gabrielperren@gmail.com>
2021-12-21 19:46:06 -0300

    testing  pyABC & pyPESTO

--- packages/best_fit/best_fit_synth_cl.py
+++ packages/best_fit/best_fit_synth_cl.py
@@ -23,6 +23,22 @@ def main(npd, pd, td, clp):
             pd['lkl_method'] in ('dolphin', 'tremmel') else pd['lkl_method']
         print("Using {} algorithm ({})".format(pd['best_fit_algor'], lkl_bin))
 
+        # # Calculate the best fitting parameters.
+        # from . import pypesto_algor
+        # isoch_fit_params = pypesto_algor.main(
+        #     clp['completeness'], clp['err_lst'],
+        #     clp['max_mag_syn'], clp['obs_clust'], pd['lkl_method'],
+        #     pd['pt_ntemps'], pd['pt_adapt'], pd['pt_tmax'],
+        #     pd['nsteps_mcee'], pd['nwalkers_mcee'], pd['mins_max'], **td)
+
+        # Calculate the best fitting parameters.
+        from . import pyabc_algor
+        isoch_fit_params = pyabc_algor.main(
+            clp['completeness'], clp['err_lst'],
+            clp['max_mag_syn'], clp['obs_clust'], pd['lkl_method'],
+            pd['pt_ntemps'], pd['pt_adapt'], pd['pt_tmax'],
+            pd['nsteps_mcee'], pd['nwalkers_mcee'], pd['mins_max'], **td)
+
         # Calculate the best fitting parameters.
         isoch_fit_params = ptemcee_algor.main(
             clp['completeness'], clp['err_lst'],
