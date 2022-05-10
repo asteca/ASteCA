70907ae8460f172af2bbe3209dfd806b5358bb1b
Gabriel <gabrielperren@gmail.com>
2022-05-10 19:37:44 -0300

    minor

--- packages/best_fit/max_mag_cut.py
+++ packages/best_fit/max_mag_cut.py
@@ -3,7 +3,7 @@ import numpy as np
 import copy
 
 
-def main(cl_reg_fit, max_mag):
+def main(cl_reg_fit, Max_mag):
     """
     Reject stars beyond the maximum magnitude given.
     """
@@ -11,24 +11,25 @@ def main(cl_reg_fit, max_mag):
     # Maximum observed (main) magnitude.
     max_mag_obs = np.max(list(zip(*list(zip(*cl_reg_fit))[1:][2]))[0])
 
-    if max_mag == 'max':
+    if Max_mag == 'max':
         # No magnitude cut applied.
         cl_max_mag, max_mag_syn = copy.deepcopy(cl_reg_fit), max_mag_obs
     else:
+        Max_mag = float(Max_mag)
         star_lst = []
         for star in cl_reg_fit:
             # Check main magnitude value.
-            if star[3][0] <= max_mag:
+            if star[3][0] <= Max_mag:
                 # Keep stars brighter that the magnitude limit.
                 star_lst.append(star)
 
         # Check number of stars left.
         if len(star_lst) > 10:
             # For the synthetic clusters, use the minimum value between the
-            # selected 'max_mag' value and the maximum observed magnitude.
-            # This prevents large 'max_mag' values from generating synthetic
+            # selected 'Max_mag' value and the maximum observed magnitude.
+            # This prevents large 'Max_mag' values from generating synthetic
             # clusters with low mass stars in the not-observed region.
-            cl_max_mag, max_mag_syn = star_lst, min(max_mag, max_mag_obs)
+            cl_max_mag, max_mag_syn = star_lst, min(Max_mag, max_mag_obs)
             print("Maximum magnitude cut applied ({:.1f} mag)".format(
                 max_mag_syn))
         else:
