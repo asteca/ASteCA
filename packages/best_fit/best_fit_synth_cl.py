8993cfe1515a3d5688d973b83c1e636e8043c49f
Gabriel <gabrielperren@gmail.com>
2022-05-13 17:45:09 -0300

    minor

--- packages/best_fit/best_fit_synth_cl.py
+++ packages/best_fit/best_fit_synth_cl.py
@@ -18,9 +18,9 @@ def main(npd, pd, td, clp):
 
     if pd['best_fit_algor'] == 'ptemcee':
         print("Searching for optimal parameters")
-        lkl_bin = pd['lkl_method'] + '; ' + pd['lkl_binning'] if\
-            pd['lkl_method'] in ('dolphin', 'tremmel') else pd['lkl_method']
-        print("Using {} algorithm ({})".format(pd['best_fit_algor'], lkl_bin))
+        # lkl_bin = pd['lkl_method'] + '; ' + pd['lkl_binning'] if\
+        #     pd['lkl_method'] in ('dolphin', 'tremmel') else pd['lkl_method']
+        # print("Using {} algorithm ({})".format(pd['best_fit_algor'], lkl_bin))
 
         clp['varIdxs'], clp['ndim'], clp['ranges'] = varPars(
             td['fundam_params'])
@@ -28,12 +28,12 @@ def main(npd, pd, td, clp):
         model_proper = np.array([_[0] for _ in td['fundam_params']])
 
         # Pack common args for the 'synth_cluster.py' function.
-        clp['syntClustArgs'] = (
-            pd['DR_dist'], pd['alpha'], model_proper,
+        clp['syntClustArgs'] = [
+            pd['DR_dist'], pd['DR_percentage'], pd['alpha'], model_proper,
             clp['varIdxs'], clp['completeness'], clp['err_lst'],
             clp['max_mag_syn'], clp['N_obs_stars'], td['fundam_params'],
             td['ext_coefs'], td['N_fc'], td['m_ini_idx'], td['st_dist_mass'],
-            td['theor_tracks'], td['rand_norm_vals'], td['rand_unif_vals'])
+            td['theor_tracks'], td['rand_norm_vals'], td['rand_unif_vals']]
 
         # import matplotlib.pyplot as plt
         # mags_cols_cl = [[], []]
@@ -42,37 +42,39 @@ def main(npd, pd, td, clp):
         # for col in list(zip(*list(zip(*clp['cl_syn_fit']))[1:][4])):
         #     mags_cols_cl[1].append(col)
         # from .bf_common import getSynthClust
-        # from ..synth_clust import move_isochrone
+        # from ..synth_clust import synth_cluster, move_isochrone
 
-        # def plot(model):
+        # def plot(model, N_obs_stars, xmin, xmax, ymin, ymax):
+        #     clp['syntClustArgs'][8] = N_obs_stars
         #     synth_clust = getSynthClust(
         #         model, True, clp['syntClustArgs'])[0].T
+        #     model_proper0, ml, mh, al, ah = synth_cluster.properModel(
+        #         td['fundam_params'], model_proper, model, clp['varIdxs'])
+        #     met, age, beta, e, dr, R_V, d = model_proper0
 
-        #     # Selected solution values for all the parameters.
-        #     am = 9.6
-        #     zm, e, dr, d, R_V = model
         #     # Values in grid
-        #     zg = np.argmin(abs(np.array(td['fundam_params'][0]) - zm))
-        #     ag = np.argmin(abs(np.array(td['fundam_params'][1]) - am))
+        #     zg = np.argmin(abs(np.array(td['fundam_params'][0]) - met))
+        #     ag = np.argmin(abs(np.array(td['fundam_params'][1]) - age))
         #     # Move isochrone
         #     isochrone = np.array(td['theor_tracks'][zg][ag])
-        #     # dr = 0
-        #     rand_v = np.ones(isochrone.shape[-1]) * .5
+        #     dr = 0
+        #     DR_percentage = 0
         #     shift_isoch = move_isochrone.main(
         #         isochrone, e, dr, d, R_V, td['ext_coefs'], td['N_fc'],
-        #         pd['DR_dist'], rand_v, rand_v, td['m_ini_idx'])
+        #         pd['DR_dist'], DR_percentage, td['rand_norm_vals'][0],
+        #         td['rand_unif_vals'][0], td['m_ini_idx'])
         #     shift_isoch = shift_isoch[:sum(td['N_fc'])]
 
         #     plt.scatter(mags_cols_cl[1][0], mags_cols_cl[0][0], alpha=.5)
-        #     plt.scatter(synth_clust[1], synth_clust[0], marker='x', alpha=.5)
+        #     plt.scatter(synth_clust[1], synth_clust[0], marker='x')
         #     plt.plot(shift_isoch[1], shift_isoch[0], c='k')
-        #     # plt.gca().invert_yaxis()
+        #     plt.gca().invert_yaxis()
         #     plt.grid(which='both')
-        #     plt.xlim(.5, 3)
-        #     plt.ylim(20.5, 10.5)
+        #     plt.xlim(xmin, xmax)
+        #     plt.ylim(ymin, ymax)
         #     plt.show()
 
-        # plot(np.array([0.01534, 0.7, 0.25, 12.1, 3.1]))
+        # plot(np.array([0.009, 9.7, 0.6, 0.2, 12.4]), 1434, .5, 2.7, 19, 11)
         # breakpoint()
 
         # from . import kombine_algor
@@ -140,23 +142,20 @@ def main(npd, pd, td, clp):
         isoch_fit_errors = params_errors(clp['varIdxs'], isoch_fit_params)
         print("Best fit parameters read from file")
 
-    # In place for #239. NOT WORKING AS OF FEB 2021, after #488 #503 #506
-    elif pd['best_fit_algor'] == 'synth_gen':
-        synth_clust_gen.main(npd, clp, pd)
-        import sys
-        sys.exit()
+    # # In place for #239. NOT WORKING AS OF FEB 2021, after #488 #503 #506
+    # elif pd['best_fit_algor'] == 'synth_gen':
+    #     synth_clust_gen.main(npd, clp, pd)
+    #     import sys
+    #     sys.exit()
 
     else:
+        # HARDCODED '7'
         print("Skip parameters fitting process")
         # Pass dummy data used by data output and some plot functions.
         isoch_fit_params = {
-            'mean_sol': [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
-            'median_sol': [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
-            'map_sol': [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
-            'mode_sol': [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
-            'param_r2': [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
-            'N_total': np.nan}
-        isoch_fit_errors = [[np.nan, np.nan, np.nan]] * 6
+            'mean_sol': [np.nan] * 7, 'median_sol': [np.nan] * 7,
+            'mode_sol': [np.nan] * 7, 'param_r2': [np.nan] * 7}
+        isoch_fit_errors = [[np.nan, np.nan, np.nan]] * 7
 
     clp['isoch_fit_params'], clp['isoch_fit_errors'] = isoch_fit_params,\
         isoch_fit_errors
@@ -206,7 +205,7 @@ def convergenceParams(
     median_sol = fillParams(fundam_params, varIdxs, median_sol)
 
     # Total number of values used to estimate the parameter's distributions.
-    N_total = mcmc_trace.shape[-1]
+    # N_total = mcmc_trace.shape[-1]
 
     isoch_fit_params.update({
         'mean_sol': mean_sol, 'median_sol': median_sol, 'mode_sol': mode_sol,
@@ -217,7 +216,7 @@ def convergenceParams(
         'all_taus': all_taus, 'acorr_function': acorr_function,
         # 'max_at_c': max_at_c, 'min_at_c': min_at_c,
         # 'minESS': minESS, 'mESS': mESS, 'mESS_epsilon': mESS_epsilon,
-        'geweke_z': geweke_z, 'mcmc_ess': mcmc_ess, 'N_total': N_total,
+        'geweke_z': geweke_z, 'mcmc_ess': mcmc_ess,
     })
 
     return isoch_fit_params
