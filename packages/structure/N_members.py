0c3085cf7e4a3d82e3bd653042c94a40c8448b78
Gabriel <gabrielperren@gmail.com>
2022-05-21 11:16:14 -0300

    updt member estimation

--- packages/structure/N_members.py
+++ packages/structure/N_members.py
@@ -11,23 +11,23 @@ def main(clp):
     function.
     """
 
-    # Field density and radius were st manually
-    if np.isnan(clp['field_dens_std']):
-        clp['n_memb'], clp['members_dist'] = 1, np.array([])
-        return clp
-
     N_in_rad = (clp['xy_cent_dist'] < clp['clust_rad']).sum()
-    field_dens_s = np.random.normal(
-        clp['field_dens'], clp['field_dens_std'], 1000)
-    N_memb_all = N_in_rad - clp['cl_area'] * field_dens_s
-    N_memb_all = N_memb_all[N_memb_all > 0]
 
-    # Used in the King profile fitting
-    if len(N_memb_all) > 10:
-        n_memb_i = int(np.median(N_memb_all))
+    # The field density was manually fixed
+    if np.isnan(clp['field_dens_std']):
+        n_memb = int(N_in_rad - clp['cl_area'] * clp['field_dens'])
+        N_memb_all = np.array([])
     else:
+        field_dens_s = np.random.normal(
+            clp['field_dens'], clp['field_dens_std'], 1000)
+        N_memb_all = N_in_rad - clp['cl_area'] * field_dens_s
+        N_memb_all = N_memb_all[N_memb_all > 0]
+        n_memb = int(np.median(N_memb_all))
+
+    if n_memb < 10:
         print("  WARNING: The estimated number of members is < 10")
-        n_memb_i = 10
-    clp['n_memb'], clp['members_dist'] = n_memb_i, N_memb_all
+        n_memb = 10
+
+    clp['n_memb'], clp['members_dist'] = n_memb, N_memb_all
 
     return clp
