fa39d78e8217aefcfbebdb2d70e370ddecab889c
Gabriel <gabrielperren@gmail.com>
2022-08-15 11:32:58 -0300

    minor, testing

--- packages/best_fit/pyabc_algor.py
+++ packages/best_fit/pyabc_algor.py
@@ -1,45 +1,43 @@
 
 import numpy as np
 import datetime as dt
-from ..synth_clust import synth_cluster
 from . import likelihood
-from .bf_common import initPop, varPars, rangeCheck, fillParams
+from .bf_common import getSynthClust
 import pyabc
 from .histodd import histogramdd
 from scipy.stats import wasserstein_distance, energy_distance, anderson_ksamp
 
 
 """
+Works fine but the uncertainties appear to be larger than those reported
+by ptemcee.
+I'm not sure how to extract the samples properly to estimate the summary
+statistics (mean, median, stddev, etc), I think I need to apply the weights
+but I'm not sure how.
+
+See: https://stats.stackexchange.com/q/555473/10416
 See also: https://stats.stackexchange.com/q/404775/10416
 """
 
 
-def main(
-    completeness, err_lst, max_mag_syn, obs_clust, lkl_method,
-    pt_ntemps, pt_adapt, pt_tmax, nsteps_mcee, nwalkers_mcee, mins_max,
-    priors_mcee, ext_coefs, binar_flag, mean_bin_mr, N_fc, m_ini_idx,
-    theor_tracks, err_norm_rand, binar_probs, ext_unif_rand, fundam_params,
-        st_dist_mass, **kwargs):
+def main(lkl_method, obs_clust, varIdxs, ndim, ranges, syntClustArgs):
     """
     """
 
     import os
     import tempfile
 
-    varIdxs, ndim, ranges = varPars(fundam_params)
-    # Pack synthetic cluster arguments.
-    synthcl_args = [
-        completeness, err_lst, max_mag_syn, ext_coefs, binar_flag,
-        mean_bin_mr, N_fc, m_ini_idx, st_dist_mass, theor_tracks,
-        err_norm_rand, binar_probs, ext_unif_rand]
+    # parameters = np.array(['z', 'a', 'B', 'Av', 'dr', 'Rv', 'dm'])
+    rvar = ranges[varIdxs]
 
     prior = pyabc.Distribution(
-        z=pyabc.RV("uniform", ranges[0][0], ranges[0][1] - ranges[0][0]),
-        a=pyabc.RV("uniform", ranges[1][0], ranges[1][1] - ranges[1][0]),
-        e=pyabc.RV("uniform", ranges[2][0], ranges[2][1] - ranges[2][0]),
-        d=pyabc.RV("uniform", ranges[3][0], ranges[3][1] - ranges[3][0]),
-        m=pyabc.RV("uniform", ranges[4][0], ranges[4][1] - ranges[4][0]),
-        b=pyabc.RV("uniform", ranges[5][0], ranges[5][1] - ranges[5][0])
+        z=pyabc.RV("uniform", rvar[0][0], rvar[0][1] - rvar[0][0]),
+        a=pyabc.RV("uniform", rvar[1][0], rvar[1][1] - rvar[1][0]),
+        B=pyabc.RV("uniform", rvar[2][0], rvar[2][1] - rvar[2][0]),
+        Av=pyabc.RV("uniform", rvar[3][0], rvar[3][1] - rvar[3][0]),
+        # dr=pyabc.RV("uniform", rvar[4][0], rvar[4][1] - rvar[4][0]),
+        # Rv=pyabc.RV("uniform", rvar[5][0], rvar[5][1] - rvar[5][0]),
+        dm=pyabc.RV("uniform", rvar[4][0], rvar[4][1] - rvar[4][0])
     )
 
     #
@@ -51,26 +49,24 @@ def main(
 
     def model(parameter):
         model_p = (
-            parameter['z'], parameter['a'], parameter['e'],
-            parameter['d'], parameter['m'], parameter['b'])
+            parameter['z'], parameter['a'], parameter['B'],
+            parameter['Av'], parameter['dm'])
         # Generate synthetic cluster.
-        synth_clust = synth_cluster.main(
-            fundam_params, varIdxs, model_p, *synthcl_args)
+        synth_clust = getSynthClust(model_p, True, syntClustArgs)[0]
         return {"y": synth_clust}
 
     def distance(synth, obs):
         return statDist(synth['y'], obs['y'], dist_pars)
 
     abc = pyabc.ABCSMC(
-        model, prior, distance, population_size=25,
-        sampler=pyabc.sampler.MulticoreEvalParallelSampler(n_procs=1))
+        model, prior, distance, population_size=1000,
+        sampler=pyabc.sampler.MulticoreEvalParallelSampler(n_procs=2))
     db_path = ("sqlite:///" + os.path.join(tempfile.gettempdir(), "pyABC.db"))
-    print(db_path)
     abc.new(db_path, obs_data)
 
     history = abc.run(
         max_nr_populations=50,  # min_eps_diff=0.001, 
-        max_walltime=dt.timedelta(hours=0, minutes=15))
+        max_walltime=dt.timedelta(hours=0, minutes=10))
 
     import matplotlib.pyplot as plt
     max_r = history.max_t
@@ -86,26 +82,17 @@ def main(
         ax.legend()
         plt.show()
 
-    makePlot("z")
+    pyabc.visualization.plot_histogram_matrix(history)
+    plt.show()
     breakpoint()
 
+    df, w = history.get_distribution()
+    makePlot("z")
     pyabc.visualization.plot_effective_sample_sizes(history)
     pyabc.visualization.plot_acceptance_rates_trajectory(history)
     pyabc.visualization.plot_eps_walltime(history)
     pyabc.visualization.plot_epsilons(history)
-    # m (int, optional (default = 0)) – Id of the model to plot for.
-    # t (int, optional (default = None, i.e. the last time)) – Time point to plot for.
-    pyabc.visualization.plot_histogram_1d(history, m=0, t=25, x='z')
-
-    isoch_fit_params = {
-        'varIdxs': varIdxs, 'ndim': ndim, 'Tmax': str(Tmax),
-        'map_sol': map_sol, 'map_lkl': map_lkl, 'map_lkl_final': map_lkl_final,
-        'prob_mean': prob_mean, 'bf_elapsed': elapsed, 'maf_allT': maf_allT,
-        'tswaps_afs': tswaps_afs, 'betas_pt': betas_pt, 'N_steps': N_steps,
-        'cold_chain': cold_chain
-    }
-
-    return isoch_fit_params
+    pyabc.visualization.plot_histogram_1d(history, x='z')
 
 
 def statDist(synth, obs, dist_pars):
@@ -115,7 +102,7 @@ def statDist(synth, obs, dist_pars):
 
     # The likelihood must be maximized so we invert (to minimize) and add a
     # constant to make the result positive
-    return 500 - likelihood.main(lkl_method, synth, [bin_edges, obs, cl_z_idx])
+    return 1e6 - likelihood.main(lkl_method, synth, [bin_edges, obs, cl_z_idx])
 
     # Histogram of the synthetic cluster, using the bin edges calculated
     # with the observed cluster.
