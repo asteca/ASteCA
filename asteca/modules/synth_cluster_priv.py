0d02f98342058fc281b64f49d7fd68a1ed75692b
Gabriel <gabrielperren@gmail.com>
2024-05-03 09:25:17 -0300

    minor

--- asteca/modules/synth_cluster_priv.py
+++ asteca/modules/synth_cluster_priv.py
@@ -145,8 +145,6 @@ def add_binarity(self) -> np.ndarray:
         N_cols: magnitude + colors + initial mass
 
     """
-    print("Generating binary data")
-
     mag_idx = 0
     N_colors = len(self.isochs.color_filter_name)
     m_ini_idx = mag_idx + N_colors + 1
@@ -197,8 +195,6 @@ def extinction_coeffs(self) -> list:
     ext_coefs = [ec_mag, ec_col1, ...]
 
     """
-    print("Obtaining extinction coefficients")
-
     # For the magnitude.
     # Effective wavelength in Armstrong.
     eff_wave = self.isochs.mag_color_lambdas[self.isochs.mag_filter_name]
@@ -216,6 +212,68 @@ def extinction_coeffs(self) -> list:
         )
 
     return ext_coefs
+
+
+def ccmo_model(mw: float) -> list:
+    """Cardelli, Clayton, and Mathis (1989 ApJ. 345, 245) model for extinction
+    coefficients with updated coefficients for near-UV from O'Donnell
+    (1994, ApJ, 422, 158).
+
+    ccm_coef = a + b / Rv
+
+    Implementation taken from:
+
+    http://idlastro.gsfc.nasa.gov/ftp/pro/astro/ccm_unred.pro
+
+    There appears to be an error in the Far-UV range in the original IDL
+    routine where the maximum inverse wavelength is 11 and it should be 10
+    according to Cardelli et al. 1989 (pag 251, Eq (5,a,b)).
+
+    """
+
+    if 0.3 <= mw < 1.1:
+        # Infrared.
+        a, b = 0.574 * (mw**1.61), -0.527 * (mw**1.61)
+
+    elif 1.1 <= mw < 3.3:
+        # Optical/NIR.
+        # Original coefficients from CCM89
+        # c1 = [1., 0.17699, -0.50447, -0.02427, 0.72085, 0.01979, -0.77530,
+        #       0.32999]
+        # c2 = [0., 1.41338, 2.28305, 1.07233, -5.38434, -0.62251, 5.30260,
+        #       -2.09002]
+        # New coefficients from O'Donnell (1994)
+        c1 = [1.0, 0.104, -0.609, 0.701, 1.137, -1.718, -0.827, 1.647, -0.505]
+        c2 = [0.0, 1.952, 2.908, -3.989, -7.985, 11.102, 5.491, -10.805, 3.347]
+        y = mw - 1.82
+        # Reverse because polyval starts from the highest degree.
+        c1.reverse(), c2.reverse()
+        a, b = np.polyval(c1, y), np.polyval(c2, y)
+
+    elif 3.3 <= mw < 8.0:
+        # Mid-UV
+        F_a, F_b = 0.0, 0.0
+        if mw >= 5.9:
+            y = mw - 5.9
+            F_a = -0.04473 * y**2 - 0.009779 * y**3
+            F_b = 0.2130 * y**2 + 0.1207 * y**3
+        a = 1.752 - 0.316 * mw - (0.104 / ((mw - 4.67) ** 2 + 0.341)) + F_a
+        b = -3.090 + 1.825 * mw + (1.206 / ((mw - 4.62) ** 2 + 0.263)) + F_b
+
+    elif 8.0 <= mw <= 10.0:
+        # Far-UV
+        c1 = [-1.073, -0.628, 0.137, -0.070]
+        c2 = [13.670, 4.257, -0.420, 0.374]
+        y = mw - 8.0
+        c1.reverse(), c2.reverse()
+        a, b = np.polyval(c1, y), np.polyval(c2, y)
+    else:
+        raise ValueError(
+            "The effective wavelength is {} [1/micron], beyond "
+            "the CCM model limit (10 [1/micron]).".format(mw)
+        )
+
+    return a, b
 
 
 def sample_imf(self, Nmets: int, Nages: int) -> list:
@@ -238,7 +296,6 @@ def sample_imf(self, Nmets: int, Nages: int) -> list:
       variety to the sampled IMF.
 
     """
-    print(f"Sampling selected IMF ({self.IMF_name})")
     inv_cdf = invTrnsfSmpl(self.IMF_name)
 
     st_dist_mass = []
@@ -387,68 +444,6 @@ def mag_combine(m1: np.ndarray, m2: np.ndarray) -> np.ndarray:
     c = 10**-0.4
     mbin = -2.5 * (-0.4 * m1 + np.log10(1.0 + c ** (m2 - m1)))
     return mbin
-
-
-def ccm_model(mw: float) -> list:
-    """Cardelli, Clayton, and Mathis (1989 ApJ. 345, 245) model for extinction
-    coefficients with updated coefficients for near-UV from O'Donnell
-    (1994, ApJ, 422, 158).
-
-    ccm_coef = a + b / Rv
-
-    Implementation taken from:
-
-    http://idlastro.gsfc.nasa.gov/ftp/pro/astro/ccm_unred.pro
-
-    There appears to be an error in the Far-UV range in the original IDL
-    routine where the maximum inverse wavelength is 11 and it should be 10
-    according to Cardelli et al. 1989 (pag 251, Eq (5,a,b)).
-
-    """
-
-    if 0.3 <= mw < 1.1:
-        # Infrared.
-        a, b = 0.574 * (mw**1.61), -0.527 * (mw**1.61)
-
-    elif 1.1 <= mw < 3.3:
-        # Optical/NIR.
-        # Original coefficients from CCM89
-        # c1 = [1., 0.17699, -0.50447, -0.02427, 0.72085, 0.01979, -0.77530,
-        #       0.32999]
-        # c2 = [0., 1.41338, 2.28305, 1.07233, -5.38434, -0.62251, 5.30260,
-        #       -2.09002]
-        # New coefficients from O'Donnell (1994)
-        c1 = [1.0, 0.104, -0.609, 0.701, 1.137, -1.718, -0.827, 1.647, -0.505]
-        c2 = [0.0, 1.952, 2.908, -3.989, -7.985, 11.102, 5.491, -10.805, 3.347]
-        y = mw - 1.82
-        # Reverse because polyval starts from the highest degree.
-        c1.reverse(), c2.reverse()
-        a, b = np.polyval(c1, y), np.polyval(c2, y)
-
-    elif 3.3 <= mw < 8.0:
-        # Mid-UV
-        F_a, F_b = 0.0, 0.0
-        if mw >= 5.9:
-            y = mw - 5.9
-            F_a = -0.04473 * y**2 - 0.009779 * y**3
-            F_b = 0.2130 * y**2 + 0.1207 * y**3
-        a = 1.752 - 0.316 * mw - (0.104 / ((mw - 4.67) ** 2 + 0.341)) + F_a
-        b = -3.090 + 1.825 * mw + (1.206 / ((mw - 4.62) ** 2 + 0.263)) + F_b
-
-    elif 8.0 <= mw <= 10.0:
-        # Far-UV
-        c1 = [-1.073, -0.628, 0.137, -0.070]
-        c2 = [13.670, 4.257, -0.420, 0.374]
-        y = mw - 8.0
-        c1.reverse(), c2.reverse()
-        a, b = np.polyval(c1, y), np.polyval(c2, y)
-    else:
-        raise ValueError(
-            "The effective wavelength is {} [1/micron], beyond "
-            "the CCM model limit (10 [1/micron]).".format(mw)
-        )
-
-    return a, b
 
 
 def properModel(
