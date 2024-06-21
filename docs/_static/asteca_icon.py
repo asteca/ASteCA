fc8b1630ffeb8283ebcc4a1e60fb378a8b8f71ff
Gabriel <gabrielperren@gmail.com>
2024-06-21 12:10:51 -0300

    data and plots for docs

--- docs/_static/asteca_icon.py
+++ docs/_static/asteca_icon.py
@@ -10,15 +10,16 @@ with open("asteca_icon.dat", "r") as f:
 cmd_xy = np.array(cmd_xy).T
 
 fig = plt.figure(figsize=(25, 25))
-ax = fig.add_subplot(111, aspect='auto')
+ax = fig.add_subplot(111, aspect="auto")
 ax.set_axis_off()
-plt.scatter(cmd_xy[0], cmd_xy[1], c='grey', s=300, edgecolor='none')
-e = Ellipse(xy=(0.77, 19.3), width=4.5, height=9, angle=0., lw=45, fc='None',
-            edgecolor='k')
+plt.scatter(cmd_xy[0], cmd_xy[1], c="grey", s=300, edgecolor="none")
+e = Ellipse(
+    xy=(0.77, 19.3), width=4.5, height=9, angle=0.0, lw=45, fc="None", edgecolor="k"
+)
 ax.add_artist(e)
-plt.xlim(-3., 6)
+plt.xlim(-3.0, 6)
 plt.ylim(28, 10)
-plt.savefig('asteca_icon.png', dpi=150, bbox_inches='tight', transparent=True)
+plt.savefig("asteca_icon.png", dpi=150, bbox_inches="tight", transparent=True)
 
 
 # # Favicon
