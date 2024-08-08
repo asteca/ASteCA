aec83f54e45fda26ff797f5eb64576a43cbb4f5c
Gabriel <gabrielperren@gmail.com>
2024-08-08 12:52:57 -0300

    testing update checker

--- asteca/__init__.py
+++ asteca/__init__.py
@@ -5,9 +5,12 @@ from .synthetic import Synthetic as synthetic
 from .likelihood import Likelihood as likelihood
 from . import plot as plot
 
+import os
 from contextlib import suppress
 import importlib.metadata
 from pathlib import Path
+import requests
+from packaging import version
 
 
 __all__ = ["cluster", "membership", "isochrones", "synthetic", "likelihood", "plot"]
@@ -18,7 +21,6 @@ def extract_version() -> str:
     found in nearby pyproject.toml
 
     https://stackoverflow.com/a/76206192/1391441
-
     """
     with suppress(FileNotFoundError, StopIteration):
         pp_file = Path(__file__).parent.parent / "pyproject.toml"
@@ -35,3 +37,21 @@ def extract_version() -> str:
 
 
 __version__ = extract_version()
+
+# Check if an updated version exists.
+# If this file exists, skip update check
+if os.path.isfile("update_off.txt") is False:
+
+    # Get the latest version from PyPI
+    pypi_url = "https://pypi.org/pypi/asteca/json"
+    pypi_response = requests.get(pypi_url, timeout=3)
+    pypi_data = pypi_response.json()
+    new_v = pypi_data["info"]["version"]
+
+    # Parse and compare versions
+    if version.parse(new_v) != version.parse(__version__):
+        print("\n--------------------------------------------------")
+        print(f"New version of ASteCA is available: {__version__} -> {new_v}")
+        print("   Update with: pip install --upgrade asteca\n")
+        print("   See what's new at: http://asteca.github.io/")
+        print("--------------------------------------------------\n")
