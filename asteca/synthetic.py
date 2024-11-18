efeb3150eda2ccc5d2eca55d461e5db7e161ce06
Gabriel <gabrielperren@gmail.com>
2024-11-18 19:33:46 -0300

    minor ruff format

--- asteca/synthetic.py
+++ asteca/synthetic.py
@@ -1,10 +1,12 @@
 import warnings
+
 import numpy as np
 import pandas as pd
+
 from .cluster import Cluster
 from .isochrones import Isochrones
-from .modules import synth_cluster_priv as scp
 from .modules import mass_binary as mb
+from .modules import synth_cluster_priv as scp
 
 
 class Synthetic:
