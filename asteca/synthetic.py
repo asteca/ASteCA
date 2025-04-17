ff5eb3f05f33e096c4ad1055d5fc746253802604
Gabriel <gabrielperren@gmail.com>
2025-04-17 10:43:40 -0300

    add for testing later

--- asteca/synthetic.py
+++ asteca/synthetic.py
@@ -336,6 +336,11 @@ class Synthetic:
         if alpha == 0.0 and beta == 0.0:
             binar_flag = False
 
+            # # TODO: this was not tested thoroughly (April 2025)
+            # # Remove binary photometry
+            # isochrone = isochrone[: self.m_ini_idx + 2]
+            # # TODO: this was not tested thoroughly
+
         # Move theoretical isochrone using the distance modulus
         isoch_moved = scp.move_isochrone(isochrone, binar_flag, self.m_ini_idx, dm)
 
