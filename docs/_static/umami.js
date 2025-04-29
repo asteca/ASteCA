3fd01601e37a900a532a5c961fe1c3d6ac46cbfb
Gabriel <gabrielperren@gmail.com>
2025-04-29 08:44:54 -0300

    testing umami script

--- docs/_static/umami.js
+++ docs/_static/umami.js
@@ -3,3 +3,4 @@ const script = document.createElement('script');
 script.defer = true;
 script.src = 'https://cloud.umami.is/script.js';
 script.setAttribute('data-website-id', '8cd88b63-caf5-4d82-98e2-8f765e390008');
+document.head.appendChild(script);
